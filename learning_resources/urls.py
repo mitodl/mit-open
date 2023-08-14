5bbbca9601e9506cb9fffca58f9e3e5f9c03247c
Matt Bertrand <mrbertrand@gmail.com>
2023-08-14 17:03:42 -0400

    Add swagger urls to learning_resources app

--- learning_resources/urls.py
+++ learning_resources/urls.py
@@ -1,5 +1,10 @@
 """Urls for channels_fields"""
 from django.urls import include, re_path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from rest_framework.routers import DefaultRouter
 
 from learning_resources.views import (
@@ -16,4 +21,17 @@ router.register(r"courses", CourseViewSet, basename="lr_courses_api")
 router.register(r"programs", ProgramViewSet, basename="lr_programs_api")
 
 
-urlpatterns = [re_path(r"^api/v1/", include(router.urls))]
+urlpatterns = [
+    re_path(r"^api/v1/", include(router.urls)),
+    re_path(r"api/v1/schema/$", SpectacularAPIView.as_view(), name="schema"),
+    re_path(
+        "api/v1/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    re_path(
+        "api/v1/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+]
