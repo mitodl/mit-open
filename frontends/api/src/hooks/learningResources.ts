130eacca0b3842b34ec24bf4231953c7a02afdf6
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-09-01 13:29:43 -0400

    add topics, learnpath api hooks

--- frontends/api/src/hooks/learningResources.ts
+++ frontends/api/src/hooks/learningResources.ts
@@ -1,19 +1,36 @@
-import { useQuery } from "@tanstack/react-query"
-import { learningResourcesApi as api } from "../clients"
+import {
+  UseQueryOptions,
+  useMutation,
+  useQuery,
+  useQueryClient
+} from "@tanstack/react-query"
+import { learningResourcesApi, learningpathsApi, topicsApi } from "../clients"
 import type {
   LearningResourcesApiLearningResourcesListRequest,
-  LearningResourcesApiLearningResourcesRetrieveRequest
+  LearningResourcesApiLearningResourcesRetrieveRequest,
+  LearningpathsApiLearningpathsCreateRequest,
+  LearningpathsApiLearningpathsPartialUpdateRequest,
+  LearningpathsApiLearningpathsDestroyRequest,
+  TopicsApiTopicsListRequest
 } from "../generated"
 import { createQueryKeys } from "@lukemorales/query-key-factory"
 
 const learningResources = createQueryKeys("learningResources", {
   detail: (params: LearningResourcesApiLearningResourcesRetrieveRequest) => ({
     queryKey: [params],
-    queryFn:  () => api.learningResourcesRetrieve(params).then(res => res.data)
+    queryFn:  () =>
+      learningResourcesApi
+        .learningResourcesRetrieve(params)
+        .then(res => res.data)
   }),
   list: (params: LearningResourcesApiLearningResourcesListRequest) => ({
     queryKey: [params],
-    queryFn:  () => api.learningResourcesList(params).then(res => res.data)
+    queryFn:  () =>
+      learningResourcesApi.learningResourcesList(params).then(res => res.data)
+  }),
+  topics: (params: TopicsApiTopicsListRequest) => ({
+    queryKey: [params],
+    queryFn:  () => topicsApi.topicsList(params).then(res => res.data)
   })
 })
 const useLearningResourcesList = (
@@ -28,4 +45,65 @@ const useLearningResourcesDetail = (
   return useQuery(learningResources.detail(params))
 }
 
-export { useLearningResourcesList, useLearningResourcesDetail }
+const useLearningResourceTopics = (
+  params: TopicsApiTopicsListRequest = {},
+  opts: Pick<UseQueryOptions, "enabled"> = {}
+) => {
+  return useQuery({
+    ...learningResources.topics(params),
+    ...opts
+  })
+}
+
+type LearningPathCreateRequest = {
+  learningPathResourceRequest: Omit<
+    LearningpathsApiLearningpathsCreateRequest["learningPathResourceRequest"],
+    "readable_id" | "resource_type"
+  >
+}
+const useLearningpathCreate = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (params: LearningPathCreateRequest) =>
+      learningpathsApi.learningpathsCreate(
+        // @ts-expect-error 'readable_id' and 'resource_type' are erroneously required
+        params
+      ),
+    onSettled: () =>
+      queryClient.invalidateQueries({
+        queryKey: learningResources._def // TODO much too agressive
+      })
+  })
+}
+const useLearningpathUpdate = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (params: LearningpathsApiLearningpathsPartialUpdateRequest) =>
+      learningpathsApi.learningpathsPartialUpdate(params),
+    onSettled: () =>
+      queryClient.invalidateQueries({
+        queryKey: learningResources._def // TODO much too agressive
+      })
+  })
+}
+
+const useLearningpathDestroy = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (params: LearningpathsApiLearningpathsDestroyRequest) =>
+      learningpathsApi.learningpathsDestroy(params),
+    onSettled: () =>
+      queryClient.invalidateQueries({
+        queryKey: learningResources._def // TODO much too agressive
+      })
+  })
+}
+
+export {
+  useLearningResourcesList,
+  useLearningResourcesDetail,
+  useLearningResourceTopics,
+  useLearningpathCreate,
+  useLearningpathUpdate,
+  useLearningpathDestroy
+}
