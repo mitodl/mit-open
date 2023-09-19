beb5003a7650767da7568b82951b15935d05b9a0
Collin Preston <collin_preston@yahoo.com>
2023-09-19 11:08:30 -0400

    Add noindex meta tag in non-prod

---
+++ open_discussions/templatetags/noindex_meta.py
@@ -0,0 +1,16 @@
+"""Adds in noindex meta tag to all pages for non-production environments."""
+from django import template
+from django.conf import settings
+from django.utils.html import format_html
+
+register = template.Library()
+
+
+@register.simple_tag()
+def noindex_meta():
+    """Add noindex for non-production environments."""
+    return (
+        format_html("""<meta name="robots" content="noindex">""")
+        if settings.ENVIRONMENT not in ("production", "prod")
+        else ""
+    )
