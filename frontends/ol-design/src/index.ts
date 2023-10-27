67a4b0eacad0ff6f544ea6ab8635451b11b53e81
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-10-27 14:46:16 -0400

    switch to just re-exporting simple components

--- frontends/ol-design/src/index.ts
+++ frontends/ol-design/src/index.ts
@@ -1,4 +1,16 @@
-export * from "./components/Button"
+/**
+ * Re-exports from MUI.
+ *
+ * This might expose more props than we want to expose. On the other hand, it
+ * means that:
+ *  - we get MUI's docstrings
+ *  - we don't need to implement ref-forwarding, which is important for some
+ *    functionality.
+ */
+export { default as Button } from "@mui/material/Button"
+export type { ButtonProps } from "@mui/material/Button"
+export { default as IconButton } from "@mui/material/IconButton"
+export type { IconButtonProps } from "@mui/material/IconButton"
+
 export * from "./components/ButtonLink"
 export * from "./components/ChipLink"
-export * from "./components/IconButton"
