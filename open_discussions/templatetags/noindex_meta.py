60613aa8fc96dc523b16818d0f50816ae16df771
Chris Chudzicki <christopher.chudzicki@gmail.com>
2023-11-17 14:24:40 -0500

    do not index any pages, for now

--- open_discussions/templatetags/noindex_meta.py
+++ open_discussions/templatetags/noindex_meta.py
@@ -1,7 +1,6 @@
 """Adds in noindex meta tag to all pages for non-production environments."""
 
 from django import template
-from django.conf import settings
 from django.utils.html import format_html
 
 register = template.Library()
@@ -12,6 +11,7 @@ def noindex_meta():
     """Add noindex for non-production environments."""
     return (
         format_html("""<meta name="robots" content="noindex">""")
-        if settings.ENVIRONMENT not in ("production", "prod")
-        else ""
+        # Restore conditional inclusion after launch
+        # if settings.ENVIRONMENT not in ("production", "prod")
+        # else ""
     )
