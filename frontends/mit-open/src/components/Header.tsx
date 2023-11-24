c9a2d1220cbf1f998895794cff4c8328e3273520
Jon <939376+jonkafton@users.noreply.github.com>
2023-11-24 16:59:41 +0100

    Replace theming and header component styles in the header with CSS-in-JS
    * Adds Emotion.js to the mit-open workspace
    
    * Replicates the glocal theme scss with object styles
    
    * Creates styled components for the header and its child components

--- frontends/mit-open/src/components/Header.tsx
+++ frontends/mit-open/src/components/Header.tsx
@@ -1,16 +1,47 @@
-import React from "react"
+import React, { FunctionComponent } from "react"
+import styled from "@emotion/styled"
 import { AppBar, Divider, Toolbar } from "ol-design"
-
 import { MITLogoLink } from "ol-util"
+import { default as Theme } from "../entry/theme"
 
-const Header: React.FC = () => {
+interface Props {
+  theme?: typeof Theme
+  position: string
+}
+
+const Bar = styled(AppBar)<Props>`
+  background-color: ${({ theme }) => theme.colorBackgroundLight};
+  color: ${({ theme }) => theme.fontColorDefault};
+  min-height: 55px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 2px 10px rgba(120 169 197 / 15%);
+`
+
+const _Toolbar = styled(Toolbar)({
+  flex: 1,
+})
+
+const LogoLink = styled(MITLogoLink)({
+  width: 45,
+  height: "auto",
+  img: {
+    height: 20,
+  },
+})
+
+const _Divider = styled(Divider)({
+  margin: "0.5em 1em",
+})
+
+const Header: FunctionComponent = () => {
   return (
-    <AppBar className="ic-header" position="sticky">
-      <Toolbar variant="dense">
-        <MITLogoLink className="ic-mit-logo" />
-        <Divider className="ic-divider" orientation="vertical" flexItem />
-      </Toolbar>
-    </AppBar>
+    <Bar position="sticky">
+      <_Toolbar variant="dense">
+        <LogoLink />
+        <_Divider orientation="vertical" flexItem />
+      </_Toolbar>
+    </Bar>
   )
 }
 
