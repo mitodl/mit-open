fb32be23f1a97d9cb44cc0d38d936c0c26973d95
Jon <939376+jonkafton@users.noreply.github.com>
2023-12-21 14:00:25 +0100

    RoutedDrawer story plus query params addon

--- frontends/mit-open/.storybook/main.ts
+++ frontends/mit-open/.storybook/main.ts
@@ -20,6 +20,7 @@ const config = {
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-queryparams"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-webpack5"),
