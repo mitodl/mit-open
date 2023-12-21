2f65990528ab459fd31e98ab64945e13c3811550
Jon <939376+jonkafton@users.noreply.github.com>
2023-12-21 16:10:49 +0100

    ShareTooltip story

---
+++ frontends/ol-components/src/components/ShareTooltip/ShareTooltip.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import type { Meta, StoryObj } from "@storybook/react"
+import { ShareTooltip } from "./ShareTooltip"
+
+const meta: Meta<typeof ShareTooltip> = {
+  title: "ol-components/ShareTooltip",
+  component: ShareTooltip,
+}
+
+export default meta
+
+type Story = StoryObj<typeof ShareTooltip>
+
+export const Simple: Story = {
+  args: {
+    url: window.location.href,
+    hideSocialButtons: false,
+    placement: "left",
+    objectType: "page",
+    children: <button>Share</button>,
+  },
+}
