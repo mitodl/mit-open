39fb9036996ed711e5ab72f4568215aa25e6183c
Jon <939376+jonkafton@users.noreply.github.com>
2024-01-10 13:22:19 +0100

    Lint fixes

--- frontends/ol-components/src/components/RadioChoiceField/RadioChoiceField.stories.tsx
+++ frontends/ol-components/src/components/RadioChoiceField/RadioChoiceField.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import type { Meta, StoryObj } from "@storybook/react"
 import { useArgs } from "@storybook/preview-api"
 import { RadioChoiceField, BooleanRadioChoiceField } from "./RadioChoiceField"
