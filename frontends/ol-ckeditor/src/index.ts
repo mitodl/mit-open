45dd27a1d981298cc42d1a5fc7ebc1f476d47c6a
Jon <939376+jonkafton@users.noreply.github.com>
2024-01-18 21:14:07 +0100

    Use stylelint-config-standard base lint rule and ignore vendor css

--- frontends/ol-ckeditor/src/index.ts
+++ frontends/ol-ckeditor/src/index.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./types/settings.d.ts" />
 
-import "./styles.css"
+import "./styles.vendor.css"
 
 export * from "./components/lazyEditors"
 export { default as CkeditorDisplay } from "./components/CkeditorDisplay"
