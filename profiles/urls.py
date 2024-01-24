d10daaa09454fe159d51e0098493643b9485ee5b
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-01-24 16:56:38 -0500

    add docs for users/me

--- profiles/urls.py
+++ profiles/urls.py
@@ -16,13 +16,18 @@ router.register(r"users", UserViewSet, basename="user_api")
 router.register(r"profiles", ProfileViewSet, basename="profile_api")
 router.register(r"websites", UserWebsiteViewSet, basename="user_websites_api")
 
-urlpatterns = [
+v0_urls = [
     re_path(
-        r"^api/v0/users/me/?$",
+        r"^/users/me/?$",
         CurrentUserRetrieveViewSet.as_view({"get": "retrieve"}),
         name="users_api-me",
     ),
-    re_path(r"^api/v0/", include(router.urls)),
+    re_path(r"", include(router.urls)),
+]
+
+app_name = "profile"
+urlpatterns = [
+    re_path("api/v0", include((v0_urls, "v0"))),
     # The URL that gravatar will redirect to if no gravatar exists for the user (no query parameters allowed).  # noqa: E501
     re_path(
         r"^profile/(?P<username>[A-Za-z0-9_]+)/(?P<size>\d+)/(?P<color>[A-Za-z0-9]+)/(?P<bgcolor>[A-Za-z0-9]+).png",
