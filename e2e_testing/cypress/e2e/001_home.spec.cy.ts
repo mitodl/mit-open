11ec85dc3052405c8ad0fb294bbc9498dabe7b4d
Jon <939376+jonkafton@users.noreply.github.com>
2024-01-24 20:20:45 +0100

    Remove log

--- e2e_testing/cypress/e2e/001_home.spec.cy.ts
+++ e2e_testing/cypress/e2e/001_home.spec.cy.ts
@@ -1,6 +1,5 @@
 describe("Home", () => {
   it("Home page loads and main elements are visible", () => {
-    console.log("PROCE", process)
     cy.visit(Cypress.env("base_url"))
 
     cy.findByRole("link", { name: "MIT Open" })
