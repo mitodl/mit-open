d6233b3e20f5d8f3fa0191ae7caa03bc1d66e8e5
Nathan Levesque <nlevesq@mit.edu>
2024-01-30 15:47:58 -0500

    Fix lint

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,8 +7,12 @@ from authentication.views import CustomLogoutView
 
 urlpatterns = [
     re_path(r"", include("social_django.urls", namespace="social")),
-    re_path(r"^login/$", RedirectView.as_view(
-        url=reverse_lazy("social:begin", args=["ol-oidc"]), query_string=True
-    ), name="login"),
+    re_path(
+        r"^login/$",
+        RedirectView.as_view(
+            url=reverse_lazy("social:begin", args=["ol-oidc"]), query_string=True
+        ),
+        name="login",
+    ),
     re_path(r"^logout/$", CustomLogoutView.as_view(), name="logout"),
 ]
