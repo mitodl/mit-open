03f60221ae8a8c4ac16bb2e9b86fb791f5603d6e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-01-30 11:33:39 -0500

    simple test

---
+++ frontends/mit-open/src/pages/DashboardPage/Dashboard.test.tsx
@@ -0,0 +1,16 @@
+import { renderTestApp, screen, waitFor } from "../../test-utils"
+
+describe("DashboardPage", () => {
+  test("Renders title", async () => {
+    renderTestApp({
+      url: `/dashboard`,
+      user: { is_authenticated: true },
+    })
+    await waitFor(() => {
+      expect(document.title).toBe("Dashboard")
+    })
+    screen.getByRole("heading", {
+      name: "Dashboard",
+    })
+  })
+})
