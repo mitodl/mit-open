60fb6a64080148959a6d0372216a9b4e70fa556f
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-01-30 10:22:57 +0100

    Clean out comments

--- e2e_testing/playwright.config.ts
+++ e2e_testing/playwright.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig, devices } from "@playwright/test"
 import "dotenv/config"
 
-// Read from default ".env" file.
-// dotenv.config()
-
-// // Alternatively, read from "../my.env" file.
-// dotenv.config({ path: path.resolve(__dirname, ".env") })
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -29,9 +18,6 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
