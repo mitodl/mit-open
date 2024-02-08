7dcd70cc479ec590f405387f69e46356fd8ed5be
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-02-08 17:26:44 +0100

    Test describe blocks

--- e2e_testing/tests/001_home.spec.ts
+++ e2e_testing/tests/001_home.spec.ts
@@ -1,24 +1,24 @@
-import { test, expect } from "@playwright/test"
+import { describe, test, expect } from "@playwright/test"
 
 const { BASE_URL } = process.env
 
-test("Home page loads and main elements are visible @sanity", async ({
-  page,
-}) => {
-  await page.goto(BASE_URL!)
+describe("Home page", () => {
+  test("Loads and main elements are visible @sanity", async ({ page }) => {
+    await page.goto(BASE_URL!)
 
-  await expect(
-    page.getByRole("link", { name: "MIT Open" }),
-    "Header link is visible",
-  ).toBeVisible()
+    await expect(
+      page.getByRole("link", { name: "MIT Open" }),
+      "Header link is visible",
+    ).toBeVisible()
 
-  await expect(
-    page.getByRole("heading", { name: "Learn from MIT" }),
-    "Main heading is visible",
-  ).toBeVisible()
+    await expect(
+      page.getByRole("heading", { name: "Learn from MIT" }),
+      "Main heading is visible",
+    ).toBeVisible()
 
-  await expect(
-    page.getByPlaceholder("What do you want to learn?"),
-    "Main search input is visible",
-  ).toBeVisible()
+    await expect(
+      page.getByPlaceholder("What do you want to learn?"),
+      "Main search input is visible",
+    ).toBeVisible()
+  })
 })
--- e2e_testing/tests/002_carousel.spec.ts
+++ e2e_testing/tests/002_carousel.spec.ts
@@ -1,51 +1,51 @@
-import { test, expect } from "@playwright/test"
+import { describe, test, expect } from "@playwright/test"
 
 const { BASE_URL } = process.env
 
-test("Home page upcoming courses carousel section renders correctly @sanity", async ({
-  page,
-}) => {
-  await page.goto(BASE_URL!)
+describe("Home page upcoming courses carousel", () => {
+  test("Page section renders correctly @sanity", async ({ page }) => {
+    await page.goto(BASE_URL!)
 
-  await expect(
-    page.getByRole("heading", { name: "Upcoming Courses" }),
-    "Upcoming courses header is visible",
-  ).toBeVisible()
-})
+    await expect(
+      page.getByRole("heading", { name: "Upcoming Courses" }),
+      "Upcoming courses header is visible",
+    ).toBeVisible()
+  })
 
-test("Home page upcoming courses carousel item renders correctly", async ({
-  page,
-}) => {
-  await expect(
-    page
-      .locator("section")
-      .filter({
-        hasText: "Upcoming Courses",
-      })
-      .getByRole("tabpanel")
-      .getByText("Program"),
-    "Resource type is visible",
-  ).toBeVisible()
+  test("Carousel item renders correctly", async ({ page }) => {
+    await page.goto(BASE_URL!)
 
-  await expect(
-    page
-      .locator("section")
-      .filter({
-        hasText: "Upcoming Courses",
-      })
-      .getByRole("tabpanel")
-      .getByRole("button", { name: "Test 100000" }),
-    "Title is visible",
-  ).toBeVisible()
+    await expect(
+      page
+        .locator("section")
+        .filter({
+          hasText: "Upcoming Courses",
+        })
+        .getByRole("tabpanel")
+        .getByText("Program"),
+      "Resource type is visible",
+    ).toBeVisible()
 
-  await expect(
-    page
-      .locator("section")
-      .filter({
-        hasText: "Upcoming Courses",
-      })
-      .getByRole("tabpanel")
-      .getByText("Offered by –MITx"),
-    "Provider is visible",
-  ).toBeVisible()
+    await expect(
+      page
+        .locator("section")
+        .filter({
+          hasText: "Upcoming Courses",
+        })
+        .getByRole("tabpanel")
+        .getByRole("button", { name: "Test 100000" }),
+      "Title is visible",
+    ).toBeVisible()
+
+    await expect(
+      page
+        .locator("section")
+        .filter({
+          hasText: "Upcoming Courses",
+        })
+        .getByRole("tabpanel")
+        .getByText("Offered by –MITx"),
+      "Provider is visible",
+    ).toBeVisible()
+  })
 })
