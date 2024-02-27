795b2315b7ec064ba3e67080914b94af3ef30c65
Collin Preston <collinpreston@users.noreply.github.com>
2024-02-27 09:36:32 -0500

    Remove livestream app (#549)

--- livestream/urls.py
+++
@@ -1,7 +0,0 @@
-"""livestream URL"""
-
-from django.urls import re_path
-
-from livestream.views import livestream_view
-
-urlpatterns = [re_path(r"^api/v0/livestream/$", livestream_view, name="livestream")]
