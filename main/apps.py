327291386d37a059cd6195da9e87205b15712195
Nathan Levesque <rhysyngsun@gmail.com>
2024-03-08 08:13:42 -0500

    Bring in posthog w/ stability improvements

--- main/apps.py
+++ main/apps.py
@@ -1,16 +1,11 @@
 from django.apps import AppConfig
 
+from main import features
 
 class MainConfig(AppConfig):
-    """
-    Main configuration
-    """
-
     default_auto_field = "django.db.models.BigAutoField"
     name = "main"
 
     def ready(self):
-        """Initialize the app"""
-        from main import features
-
         features.configure()
+
