8d0670cecc2a4367896ddc4f0ae11f7f09c4da68
Nathan Levesque <rhysyngsun@gmail.com>
2024-03-08 08:13:42 -0500

    Bring in posthog w/ stability improvements

--- main/settings.py
+++ main/settings.py
@@ -686,3 +686,16 @@ KEYCLOAK_REALM_NAME = get_string(
 )
 
 MICROMASTERS_CMS_API_URL = get_string("MICROMASTERS_CMS_API_URL", None)
+
+POSTHOG_ENABLED = get_bool(
+    name="POSTHOG_ENABLED",
+    default=False,
+)
+POSTHOG_PROJECT_API_KEY = get_string(
+    name="POSTHOG_PROJECT_API_KEY",
+    default="",
+)
+POSTHOG_API_HOST = get_string(
+    name="POSTHOG_API_HOST",
+    default="https://us.posthog.com",
+)
