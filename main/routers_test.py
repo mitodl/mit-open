07d2eb6bf7666c25788abab33f0c2fc300da3e5b
shankar ambady <ambady@mit.edu>
2024-03-15 13:24:52 -0400

    adding doc

--- main/routers_test.py
+++ main/routers_test.py
@@ -5,5 +5,8 @@ from profiles.factories import ProgramCertificateFactory
 
 
 def test_external_tables_are_readonly():
+    """
+    Test that external tables cannot be written to
+    """
     with pytest.raises(ReadOnlyModelError):
         ProgramCertificateFactory(user_email="test@test.com", program_title="test")
