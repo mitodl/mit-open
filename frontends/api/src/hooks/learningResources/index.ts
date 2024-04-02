0d8ae23bbb0960359399f0a049302351175d4c9d
Carey Gumaer <gumaerc@mit.edu>
2024-04-02 13:30:06 -0400

    add hooks for creating, updating and deleting UserList

--- frontends/api/src/hooks/learningResources/index.ts
+++ frontends/api/src/hooks/learningResources/index.ts
@@ -19,8 +19,11 @@ import type {
   LearningResource,
   LearningResourcesSearchApiLearningResourcesSearchRetrieveRequest as LRSearchRequest,
   UserlistsApiUserlistsListRequest as ULListRequest,
+  UserlistsApiUserlistsCreateRequest as ULCreateRequest,
+  UserlistsApiUserlistsDestroyRequest as ULDestroyRequest,
   UserlistsApiUserlistsItemsListRequest as ULItemsListRequest,
   OfferorsApiOfferorsListRequest,
+  UserList,
 } from "../../generated/v1"
 import learningResources, { invalidateResourceQueries } from "./keyFactory"
 import { ListType } from "../../common/constants"
@@ -220,6 +223,47 @@ const useUserListsDetail = (id: number) => {
   return useQuery(learningResources.userlists._ctx.detail(id))
 }
 
+type UserListCreateRequest = Omit<
+  ULCreateRequest["UserListRequest"],
+  "readable_id" | "resource_type"
+>
+const useUserListCreate = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (params: UserListCreateRequest) =>
+      userListsApi.userlistsCreate({
+        UserListRequest: params,
+      }),
+    onSettled: () => {
+      queryClient.invalidateQueries(learningResources.userlists._ctx.list._def)
+    },
+  })
+}
+const useUserListUpdate = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (params: Pick<UserList, "id"> & Partial<UserList>) =>
+      userListsApi.userlistsPartialUpdate({
+        id: params.id,
+        PatchedUserListRequest: params,
+      }),
+    onSettled: (_data, _err, vars) => {
+      invalidateResourceQueries(queryClient, vars.id)
+    },
+  })
+}
+
+const useUserListDestroy = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (params: ULDestroyRequest) =>
+      learningpathsApi.learningpathsDestroy(params),
+    onSettled: (_data, _err, vars) => {
+      invalidateResourceQueries(queryClient, vars.id)
+    },
+  })
+}
+
 const useInfiniteUserListItems = (
   params: ULItemsListRequest,
   options: Pick<UseQueryOptions, "enabled"> = {},
@@ -306,6 +350,9 @@ export {
   useLearningResourcesSearch,
   useUserListList,
   useUserListsDetail,
+  useUserListCreate,
+  useUserListUpdate,
+  useUserListDestroy,
   useInfiniteUserListItems,
   useOfferorsList,
   useListItemMove,
