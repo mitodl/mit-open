84f48b401fea47939a2e39f212073700e0b19b81
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-04-02 20:00:54 -0400

    start simplifying carousel

--- frontends/ol-components/src/components/TitledCarousel/TitledCarousel.tsx
+++ frontends/ol-components/src/components/TitledCarousel/TitledCarousel.tsx
@@ -1,11 +1,13 @@
-import React, { ElementType, useCallback, useRef, useState } from "react"
-import Carousel from "nuka-carousel"
+import React, { ElementType, useCallback, useState } from "react"
+import NukaCarousel from "nuka-carousel"
 import { clamp } from "lodash"
-import type { CarouselProps } from "nuka-carousel"
+import type { CarouselProps as NukaCarouselProps } from "nuka-carousel"
 import styled from "@emotion/styled"
-import { theme } from "../ThemeProvider/ThemeProvider"
+import NavigateNextIcon from "@mui/icons-material/NavigateNext"
+import { FilledIconButton } from "../FilledIconButton/FilledIconButton"
+import Stack from "@mui/material/Stack"
 
-type TitledCarouselProps = {
+type CarouselProps = {
   children: React.ReactNode
   title?: React.ReactNode
   as?: ElementType
@@ -16,7 +18,7 @@ type TitledCarouselProps = {
    * Animation duration in milliseconds.
    */
   animationDuration?: number
-  cellSpacing?: CarouselProps["cellSpacing"]
+  cellSpacing?: NukaCarouselProps["cellSpacing"]
   /**
    * React element to use as "Previous Page" button.
    *
@@ -35,75 +37,53 @@ type TitledCarouselProps = {
   showNavigationButtons?: boolean
 }
 
-const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const ButtonsContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`
-
 const CAROUSEL_SPACING = 24
 
-const StyledCarousel = styled(Carousel)({
-  paddingTop: theme.custom.shadowOverflowTop,
-  paddingBottom: theme.custom.shadowOverflowBottom,
-
+const NukaCarouselStyled = styled(NukaCarousel)({
   /*
     We want the carousel cards to:
-      1. be spaced
-      2. cast a shadow when hovered
+      1. be spaced,
+      2. have shadows (possibly), and
       3. be left-aligned (left edge of left-most card aligned with rest of page content)
 
     The card container has `overflow: hidden` to prevent seeing the offscreen
     cards. Consequently, if the leftmost card is at the left edge of the carousel
     container, then its shadow gets cut off and looks weird.
 
-    So instead: keep a margin on the left-most card, but translate the whole
-    carousel leftwards by the same margin. This keeps stuff spaced and makes the
-    shadows look nice.
+    So instead:
+      1. Use the default NukaCarousel behavior where there is half a cellSpacing
+        of padding on the left and right of each slide
+      2. translate the contents leftwards by half a cellSpacing so that they
+        appear left-aligned
+      3. Increase the width to 100% + cellSpacing so that the right-most card
+        is right-aligned
+      4. Apply positive-padding, negative-margin to the top and bottom to allow
+        vertical shadows.
 
-    Caveat: This is not a good solution if there is content within $carouselSpacing
-    of the carousel's left edge. But...there's not.
+    NOTE: This will not work if the horizontal shadow exceeds half the
+    cellspacing. In that case, the horizontal shadow will be cut off.
     */
-  transform: `translateX(-${CAROUSEL_SPACING}px * 0.5)`,
-
-  /*
-    We also want the carousel contents (cards) to appear as if they are full
-    width. By default, the width is 100% and since there's cell-spacing, the
-    right-most card does not line up with the right-hand margin, it's short by
-    half the spacing.
-
-    But since we also translated the contents leftwards (see above) we're actually
-    off by a full cellSpacing.
-
-    This needs to be !important because Nuke Carousel applies width: 100% as an
-    inline style.
-    */
-  width: `calc(100% + #{${CAROUSEL_SPACING}px}) !important`,
+  transform: `translateX(-${CAROUSEL_SPACING * 0.5}px)`,
+  width: `calc(100% + ${CAROUSEL_SPACING}px) !important`,
+  /**
+   * These values are a bit arbitrary. They just need to exceed the vertical
+   * shadow.
+   */
+  paddingBottom: "6px",
+  marginBottom: "-6px",
+  paddingTop: "6px",
+  marginTop: "-6px",
 })
 
 const defaultAnimationDuration = 800
 
-const TitledCarousel: React.FC<TitledCarouselProps> = ({
+const Carousel: React.FC<CarouselProps> = ({
   children,
-  title,
   className,
-  headerClassName,
   pageSize,
-  cellSpacing,
   animationDuration = defaultAnimationDuration,
   as: ContainerComponent = "div",
-  previous = <button>Previous</button>,
-  next = <button>Next</button>,
-  showNavigationButtons = true,
 }) => {
-  const wasButtonChange = useRef(false)
   const [index, setIndex] = useState(0)
   const childCount = React.Children.count(children)
   const canPageUp = index + pageSize < childCount
@@ -113,55 +93,48 @@ const TitledCarousel: React.FC<TitledCarouselProps> = ({
     setIndex((currentIndex) =>
       clamp(currentIndex - pageSize, 0, childCount - 1),
     )
-    wasButtonChange.current = true
   }, [pageSize, childCount])
   const pageUp = useCallback(() => {
     setIndex((currentIndex) =>
       clamp(currentIndex + pageSize, 0, childCount - 1),
     )
-    wasButtonChange.current = true
   }, [pageSize, childCount])
-  const syncIndexFromDrag = useCallback((sliderIndex: number) => {
-    if (!wasButtonChange.current) {
-      /**
-       * This was a drag change, so we need to manually sync the index with our
-       * state.
-       */
-      setIndex(sliderIndex)
-    }
-    wasButtonChange.current = false
-  }, [])
+  const handleBeforeSlide: NonNullable<NukaCarouselProps["beforeSlide"]> =
+    useCallback((_currentIndex, endIndex) => {
+      setIndex(endIndex)
+    }, [])
 
   return (
-    <ContainerComponent className={className}>
-      <HeaderContainer className={headerClassName}>
-        {title}
-        {showNavigationButtons && (
-          <ButtonsContainer>
-            {React.cloneElement(previous, {
-              disabled: !canPageDown,
-              onClick: pageDown,
-            })}
-            {React.cloneElement(next, {
-              disabled: !canPageUp,
-              onClick: pageUp,
-            })}
-          </ButtonsContainer>
-        )}
-      </HeaderContainer>
-      <StyledCarousel
+    <ContainerComponent id="hello" className={className}>
+      <NukaCarouselStyled
         slideIndex={index}
         slidesToShow={pageSize}
-        afterSlide={syncIndexFromDrag}
-        cellSpacing={cellSpacing}
+        beforeSlide={handleBeforeSlide}
         withoutControls={true}
+        cellSpacing={CAROUSEL_SPACING}
         speed={animationDuration}
       >
         {children}
-      </StyledCarousel>
+      </NukaCarouselStyled>
+      <Stack flexDirection="row" justifyContent="end" gap={3}>
+        <FilledIconButton
+          variant="contained"
+          onClick={pageDown}
+          disabled={!canPageDown}
+        >
+          <NavigateNextIcon fontSize="large" />
+        </FilledIconButton>
+        <FilledIconButton
+          variant="contained"
+          onClick={pageUp}
+          disabled={!canPageUp}
+        >
+          <NavigateNextIcon fontSize="large" />
+        </FilledIconButton>
+      </Stack>
     </ContainerComponent>
   )
 }
 
-export { TitledCarousel }
-export type { TitledCarouselProps }
+export { Carousel as TitledCarousel }
+export type { CarouselProps as TitledCarouselProps }
