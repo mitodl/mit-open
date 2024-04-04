1f35112a3e6a5df6645023413135ae90115e2e7c
James Kachel <jkachel@mit.edu>
2024-04-04 11:02:42 -0500

    formatting, still won't pass pre-commit

--- frontends/mit-open/src/components/PostHogView/PostHogView.tsx
+++ frontends/mit-open/src/components/PostHogView/PostHogView.tsx
@@ -6,27 +6,32 @@ import type { LearningResource } from "api"
 import { RoutedDrawer } from "ol-components"
 import type { RoutedDrawerProps } from "ol-components"
 
-const PostHogView: React.FC<RoutedDrawerProps> = <K extends string, R extends K = K>(
-    props: RoutedDrawerProps<K, R>,
-  ) => {
-    const { posthog: posthogSettings } = window.SETTINGS
-    const posthog = usePostHog()
+const PostHogView: React.FC<RoutedDrawerProps> = <
+  K extends string,
+  R extends K = K,
+>(
+  props: RoutedDrawerProps<K, R>,
+) => {
+  const { posthog: posthogSettings } = window.SETTINGS
+  const posthog = usePostHog()
 
-    const hndOnView = async (params: readonly K[]) => {
-        /* 
+  const hndOnView = async (params: readonly K[]) => {
+    /*
         Notes to self: this seems like a reliable way to both use the posthog provider
         and also async hit the thing, so we need to now load the data about the resource
         from the query client and hit the pageview event
         */
-        const ourParams = params as Record<K, string>
+    const ourParams = params as Record<K, string>
 
-        if (posthogSettings?.enabled) {
-            console.log("our resource is", params)
-            posthog.capture("lrd_view", { resourceId: ourParams["resource"] || "unknown"})
-        }
+    if (posthogSettings?.enabled) {
+      console.log("our resource is", params)
+      posthog.capture("lrd_view", {
+        resourceId: ourParams["resource"] || "unknown",
+      })
     }
+  }
 
-    return <RoutedDrawer {...props} onView={hndOnView} />
+  return <RoutedDrawer {...props} onView={hndOnView} />
 }
 
 export default PostHogView
