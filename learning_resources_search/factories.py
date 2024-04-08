01e5a59f6a3ec8f592213e3acd6e0f6c80966914
shankar ambady <ambady@mit.edu>
2024-04-08 13:57:19 -0400

    removing undefined model

--- learning_resources_search/factories.py
+++ learning_resources_search/factories.py
@@ -4,10 +4,8 @@ from learning_resources_search import models
 
 
 class PercolateQueryFactory(DjangoModelFactory):
+    original_query = {"test": "test"}
+    query = {"test": "test"}
+
     class Meta:
         model = models.PercolateQuery
-
-
-class PercolateQueryUser(DjangoModelFactory):
-    class Meta:
-        model = models.PercolateQueryUser
