b1c741cb45f69cb2dd4ee045fe2006053bccfda8
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-04-18 14:36:36 -0400

    revert recent theme input changes

--- frontends/ol-components/src/components/ThemeProvider/inputs.ts
+++ frontends/ol-components/src/components/ThemeProvider/inputs.ts
@@ -1,32 +1,9 @@
 import type { ThemeOptions } from "@mui/material/styles"
-import { pxToRem, typography } from "./typography"
 
 const inputBaseComponent: NonNullable<
   ThemeOptions["components"]
 >["MuiInputBase"] = {
   defaultProps: { size: "medium" },
-  styleOverrides: {
-    input: {
-      padding: "0px",
-    },
-  },
-  variants: [
-    {
-      props: { size: "hero" },
-      style: {
-        padding: "8px 16px",
-        height: pxToRem(56),
-      },
-    },
-    {
-      props: { size: "medium" },
-      style: {
-        padding: "8px 12px",
-        height: pxToRem(40),
-        ...typography.body2,
-      },
-    },
-  ],
 }
 
 const outlinedInputComponent: NonNullable<
@@ -35,9 +12,6 @@ const outlinedInputComponent: NonNullable<
   styleOverrides: {
     root: {
       backgroundColor: "white",
-    },
-    input: {
-      padding: "0px",
     },
   },
 }
