9c2bcd56c6ffdf3117010e8efaef3f3b1ba6327a
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-05-01 21:09:45 +0200

    Alert component and Storybook stories

---
+++ frontends/ol-components/src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,81 @@
+import React from "react"
+import type { Meta, StoryObj } from "@storybook/react"
+import { Alert } from "./Alert"
+import Stack from "@mui/material/Stack"
+
+const meta: Meta<typeof Alert> = {
+  title: "smoot-design/Alert",
+  component: Alert,
+  argTypes: {
+    severity: {
+      options: ["success", "info", "warning", "error"],
+      control: {
+        type: "select",
+      },
+    },
+    visible: {
+      control: {
+        type: "boolean",
+      },
+    },
+    closeable: {
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Alert>
+
+export const Plain: Story = {
+  args: {
+    severity: "success",
+  },
+  render: (args) => (
+    <Alert {...args}>Alert with severity "{args.severity}"</Alert>
+  ),
+}
+
+export const Closable: Story = {
+  args: {
+    severity: "success",
+    closeable: true,
+  },
+  render: (args) => (
+    <Alert {...args}>Closable alert with severity "{args.severity}"</Alert>
+  ),
+}
+
+export const Variants: Story = {
+  render: (args) => (
+    <Stack direction="column" gap={2} sx={{ my: 2 }}>
+      <Alert {...args} severity="success">
+        Alert with severity "success"
+      </Alert>
+      <Alert {...args} severity="info">
+        Alert with severity "info"
+      </Alert>
+      <Alert {...args} severity="warning">
+        Alert with severity "warning"
+      </Alert>
+      <Alert {...args} severity="error">
+        Alert with severity "error"
+      </Alert>
+      <Alert {...args} closeable severity="success">
+        Closeable alert with severity "success"
+      </Alert>
+      <Alert {...args} closeable severity="info">
+        Closeable alert with severity "info"
+      </Alert>
+      <Alert {...args} closeable severity="warning">
+        Closeable alert with severity "warning"
+      </Alert>
+      <Alert {...args} closeable severity="error">
+        Closeable alert with severity "error"
+      </Alert>
+    </Stack>
+  ),
+}
