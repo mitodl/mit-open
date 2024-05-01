bf6fa6afec32bddbfbe074d5b7d578e862d967d5
Carey Gumaer <gumaerc@mit.edu>
2024-05-01 17:53:08 -0400

    use readable_id in the exported user list dialog component

--- frontends/mit-open/src/ExportedComponents.tsx
+++ frontends/mit-open/src/ExportedComponents.tsx
@@ -1,25 +1,38 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { AddToUserListDialog } from "@/page-components/LearningResourceCard/AddToListDialog"
+import { AddToUserListDialogInner } from "@/page-components/LearningResourceCard/AddToListDialog"
 import NiceModal from "@ebay/nice-modal-react"
 import { createQueryClient } from "@/services/react-query/react-query"
 import ExportedComponentProviders from "@/ExportedComponentProviders"
+import { useLearningResourcesList } from "api/hooks/learningResources"
 
-const prepareModals = (container: HTMLElement) => {
+const initMitOpenDom = (container: HTMLElement) => {
   const root = createRoot(container)
-
   const queryClient = createQueryClient()
-
-  root.render(<ExportedComponentProviders queryClient={queryClient} />)
+  return root.render(<ExportedComponentProviders queryClient={queryClient} />)
 }
 
-const openAddToListDialog = (resourceId: number) => {
-  NiceModal.show(AddToUserListDialog, { resourceId })
+type ExportedDialogProps = {
+  readableId: string
 }
 
-// // @ts-expect-error
-// window.prepareModals = prepareModals
-// // @ts-expect-error
-// window.openAddToListDialog = openAddToListDialog
+const AddToUserListDialogComponent: React.FC<ExportedDialogProps> = ({
+  readableId,
+}) => {
+  const resourcesQuery = useLearningResourcesList({ readable_id: [readableId] })
+  const response = resourcesQuery.data ?? null
+  if (response === null) return null
+  const resourceId = response.results[0].id
+  return (
+    <AddToUserListDialogInner
+      resourceId={resourceId}
+    ></AddToUserListDialogInner>
+  )
+}
 
-export { prepareModals, openAddToListDialog }
+const openAddToUserListDialog = (readableId: string) => {
+  const addToUserListDialog = NiceModal.create(AddToUserListDialogComponent)
+  NiceModal.show(addToUserListDialog, { readableId })
+}
+
+export { initMitOpenDom, openAddToUserListDialog }
