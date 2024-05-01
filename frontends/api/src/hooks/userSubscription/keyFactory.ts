b75a42804694b2ea7809539e7fe614227b4f93d9
shankar ambady <ambady@mit.edu>
2024-05-01 12:51:08 -0400

    adding subscribe hooks

---
+++ frontends/api/src/hooks/userSubscription/keyFactory.ts
@@ -0,0 +1,15 @@
+import { searchSubscriptionApi } from "../../clients"
+import { createQueryKeys } from "@lukemorales/query-key-factory"
+import type { LearningResourcesUserSubscriptionApiLearningResourcesUserSubscriptionListRequest as subscriptionListRequest } from "../../generated/v1"
+const searchSubscriptions = createQueryKeys("searchSubscriptions", {
+  list: (params: subscriptionListRequest) => ({
+    queryKey: [params],
+    queryFn: () => {
+      return searchSubscriptionApi
+        .learningResourcesUserSubscriptionList(params)
+        .then((res) => res.data)
+    },
+  }),
+})
+
+export default searchSubscriptions
