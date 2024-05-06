9a65610d0dd7cae84ab902d4b5f97fb890cdc543
Carey Gumaer <gumaerc@mit.edu>
2024-05-06 15:32:08 -0400

    remove inline bypass of extraneous dependency check in webpack.exports.js and instead ignore the whole file in .eslintrc.js

--- frontends/mit-open/webpack.exports.js
+++ frontends/mit-open/webpack.exports.js
@@ -1,5 +1,4 @@
 const path = require("path")
-// eslint-disable-next-line import/no-extraneous-dependencies
 const webpackMerge = require("webpack-merge")
 const config = require("./webpack.config")
 
