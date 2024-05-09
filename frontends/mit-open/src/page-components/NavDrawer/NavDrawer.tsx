f5a0031de276ac404effbb194b5eefb1e16a9252
Carey Gumaer <gumaerc@mit.edu>
2024-05-09 19:00:21 -0400

    add "with certificate" link

--- frontends/mit-open/src/page-components/NavDrawer/NavDrawer.tsx
+++ frontends/mit-open/src/page-components/NavDrawer/NavDrawer.tsx
@@ -157,6 +157,7 @@ const navData: NavData = {
         },
         {
           title: "With Certificate",
+          href: urls.querifiedSearchUrl({ certification: "true" }),
         },
       ],
     },
