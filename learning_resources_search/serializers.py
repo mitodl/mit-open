d655e42bc854605ff6a6b6338b4bb56342e8f19c
Matt Bertrand <mrbertrand@gmail.com>
2024-05-22 16:31:39 -0400

    professional courses are never free

--- learning_resources_search/serializers.py
+++ learning_resources_search/serializers.py
@@ -68,7 +68,9 @@ def serialize_learning_resource_for_update(
         LearningResourceType.program.name,
     ]:
         prices = learning_resource_obj.prices
-        serialized_data["free"] = Decimal(0.00) in prices or not prices or prices == []
+        serialized_data["free"] = not learning_resource_obj.professional and (
+            Decimal(0.00) in prices or not prices or prices == []
+        )
     else:
         serialized_data["free"] = True
     if learning_resource_obj.resource_type == LearningResourceType.course.name:
