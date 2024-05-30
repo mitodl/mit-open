3467d4df45529a4354d02cd505616a48b9a9f995
James Kachel <jkachel@mit.edu>
2024-05-30 13:44:11 -0500

    Removing deprecated stuff from the testimonials factories

--- frontends/api/src/test-utils/factories/testimonials.ts
+++ frontends/api/src/test-utils/factories/testimonials.ts
@@ -4,14 +4,14 @@ import type { Factory } from "ol-test-utilities"
 import type { Attestation } from "../../generated/v0"
 
 const testimonial: Factory<Attestation> = (overrides = {}) => ({
-  id: faker.datatype.number(),
+  id: faker.number.int(),
   title: faker.lorem.sentence(),
   quote: faker.lorem.paragraph(),
-  attestant_name: faker.name.fullName(),
-  avatar: faker.image.imageUrl(500, 500),
-  cover: faker.image.imageUrl(),
-  avatar_medium: faker.image.imageUrl(90),
-  avatar_small: faker.image.imageUrl(22),
+  attestant_name: faker.person.fullName(),
+  avatar: faker.image.url({ width: 500, height: 500 }),
+  cover: faker.image.url(),
+  avatar_medium: faker.image.url({ width: 90, height: 90 }),
+  avatar_small: faker.image.url({ width: 22, height: 22 }),
   created_on: faker.date.past().toISOString(),
   updated_on: faker.date.past().toISOString(),
   publish_date: null,
