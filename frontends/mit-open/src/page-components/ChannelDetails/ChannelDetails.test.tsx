4646b6a3b5c6e0f2fc87d61e1e47e38b29153f9d
Shankar Ambady <shanbady@gmail.com>
2024-06-04 11:48:15 -0400

    Shanbady/additional details on offeror channel pages (#975)
    * adding component skeleton
    
    * inheriting from channel details template
    
    * adding component to field pages
    
    * layout changes and data display
    
    * exporting box
    
    * adding dividers
    
    * fixing theme
    
    * removing console logs and minor visual tweaks
    
    * patch for test
    
    * patch for test
    
    * fixing tests
    
    * fixing typechecks
    
    * fixing typechecks
    
    * fixing test
    
    * fixing styling
    
    * styling fix
    
    * visual tweaks
    
    * cosmetic changes
    
    * cosmetic changes
    
    * fixing lint
    
    * addressing feedback. minor visual tweaks
    
    * visual tweaks
    
    * tweaks
    
    * tweaks
    
    * tweaks
    
    * style fixes

---
+++ frontends/mit-open/src/page-components/ChannelDetails/ChannelDetails.test.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { ChannelDetails } from "./ChannelDetails"
+import { BrowserRouter } from "react-router-dom"
+import { urls } from "api/test-utils"
+import { setMockResponse } from "../../test-utils"
+import { fields as factory } from "api/test-utils/factories"
+import { ThemeProvider } from "ol-components"
+
+describe("ChannelDetails", () => {
+  it("Includes channel detail info panel", async () => {
+    const field = factory.field({
+      title: "Test Title",
+      channel_type: "offeror",
+    })
+    setMockResponse.get(
+      urls.fields.details(field.channel_type, field.name),
+      field,
+    )
+    render(
+      <BrowserRouter>
+        <ChannelDetails field={field} />
+      </BrowserRouter>,
+      { wrapper: ThemeProvider },
+    )
+    const fieldData = field as unknown as Record<string, unknown>
+    const offerorDetail = fieldData.offeror_detail as unknown as Record<
+      string,
+      unknown
+    >
+    const offeror = offerorDetail.offeror as unknown as Record<string, unknown>
+    const offerings = offeror.offerings as string[]
+    const audience = offeror.audience as string[]
+    const formats = offeror.formats as string[]
+    const contentTypes = offeror.content_types as string[]
+    const certifications = offeror.certifications as string[]
+    screen.getByText(offerings.join(" | "))
+    screen.getByText(audience.join(" | "))
+    screen.getByText(formats.join(" | "))
+    screen.getByText(certifications.join(" | "))
+    screen.getByText(contentTypes.join(" | "))
+  })
+})
