590b4bda1caca49d0f6e83ade2fac31cb1cd2161
James Kachel <jkachel@mit.edu>
2024-06-04 10:12:31 -0500

    Pausing - updated the card to pull channel info
    Stopping here because the API needs a touch bit of work - we want to be able to attach attestations to subbrands and that will be a necessary thing for this so stopping to get that in.

--- frontends/mit-open/src/page-components/TestimonialCard/TestimonialCard.tsx
+++ frontends/mit-open/src/page-components/TestimonialCard/TestimonialCard.tsx
@@ -1,7 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-import { styled, theme } from "ol-components"
-// import { useTestimonialList } from "api/hooks/testimonials"
+import { styled } from "ol-components"
+import { useTestimonialList } from "api/hooks/testimonials"
+import { Attestation } from "api/v0"
+
+type TestimonialCardProps = {
+  channels?: Array<number>
+}
+
+type TestimonialCardDataProps = {
+  channels?: Array<number>
+  children: ({
+    resources,
+    isLoading,
+  }: {
+    resources: Attestation[]
+    isLoading: boolean
+  }) => React.ReactNode
+}
+
+type InternalTestimonialCardProps = {
+  resources: Attestation[]
+  isLoading: boolean
+}
 
 const QuoteContainer = styled.section(({ theme }) => ({
   backgroundColor: theme.custom.colors.darkGray1,
@@ -36,7 +57,7 @@ const QuoteLeader = styled.div({
   lineHeight: "26px",
 })
 
-const Attestation = styled.div(() => ({
+const AttestationBody = styled.div(() => ({
   width: "837px",
 }))
 
@@ -48,10 +69,35 @@ const Attestant = styled.div(() => ({
   whiteSpace: "nowrap",
 }))
 
-const TestimonialCard: React.FC = () => {
-  // const { isLoading, data: testimonials } = useTestimonialList()
-  const isLoading = true
-  const testimonials: Array<any> = []
+const TestimonialCardFilteredData: React.FC<TestimonialCardDataProps> = ({
+  children,
+  channels,
+}) => {
+  const { isLoading, data } = useTestimonialList({ channels: channels })
+  return children({ resources: data?.results ?? [], isLoading })
+}
+
+const TestimonialCardData: React.FC<TestimonialCardDataProps> = ({
+  children,
+}) => {
+  const { isLoading, data } = useTestimonialList()
+  return children({ resources: data?.results ?? [], isLoading })
+}
+
+const InteriorTestimonialCard: React.FC<InternalTestimonialCardProps> = ({
+  resources,
+  isLoading,
+}) => {
+  const [randomTestimonial, setRandomTestimonial] =
+    useState<Attestation | null>(null)
+
+  useEffect(() => {
+    if (!isLoading) {
+      setRandomTestimonial(
+        resources[Math.floor(Math.random() * resources.length)],
+      )
+    }
+  }, [resources, isLoading])
 
   return isLoading ? (
     <QuoteContainer>
@@ -60,17 +106,39 @@ const TestimonialCard: React.FC = () => {
           <QuoteLeaderContainer>
             <QuoteLeader>“</QuoteLeader>
           </QuoteLeaderContainer>
-          <Attestation>
-            {testimonials[0]?.quote ??
+          <AttestationBody>
+            {randomTestimonial?.quote ??
               "You have three powerful forces coming together again, the HBCUs, and MIT, Merlot. The need is worldwide for people of all races, underrepresented populations to be able to access information, and to tap in to rich cultural resources to help them in their academic journey."}
-          </Attestation>
+          </AttestationBody>
           <Attestant>
-            {testimonials[0]?.attestant_name ?? "Dr. Robbie Melton"}
+            {randomTestimonial?.attestant_name ?? "Dr. Robbie Melton"}
           </Attestant>
         </QuoteBlock>
       </Quote>
     </QuoteContainer>
   ) : null
+}
+
+const TestimonialCard: React.FC<TestimonialCardProps> = ({ channels }) => {
+  return channels ? (
+    <TestimonialCardFilteredData channels={channels}>
+      {({ resources, isLoading }) => (
+        <InteriorTestimonialCard
+          resources={resources}
+          isLoading={isLoading}
+        ></InteriorTestimonialCard>
+      )}
+    </TestimonialCardFilteredData>
+  ) : (
+    <TestimonialCardData>
+      {({ resources, isLoading }) => (
+        <InteriorTestimonialCard
+          resources={resources}
+          isLoading={isLoading}
+        ></InteriorTestimonialCard>
+      )}
+    </TestimonialCardData>
+  )
 }
 
 export default TestimonialCard
