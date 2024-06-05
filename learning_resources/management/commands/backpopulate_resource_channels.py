de03b2a61f3d9d8034ae9e4e2a07e5ed4a167ff9
shankar ambady <ambady@mit.edu>
2024-06-04 20:54:06 -0400

    updating resource channel backpopulate command to load default template data

--- learning_resources/management/commands/backpopulate_resource_channels.py
+++ learning_resources/management/commands/backpopulate_resource_channels.py
@@ -1,8 +1,13 @@
 """Management command to create channels for topics, departments, offerors"""
 
+import json
+from pathlib import Path
+
+from django.conf import settings
 from django.core.management import BaseCommand
 
 from channels.constants import ChannelType
+from channels.models import FieldChannel
 from learning_resources.hooks import get_plugin_manager
 from learning_resources.models import (
     LearningResourceDepartment,
@@ -47,11 +52,36 @@ class Command(BaseCommand):
             )
         super().add_arguments(parser)
 
+    def get_offeror_template_config(self):
+        with Path.open(
+            settings.BASE_DIR
+            + "/learning_resources/data/channel_templates/offerors.json"
+        ) as f:
+            return json.load(f)
+
+    def set_default_offeror_template(self, offeror_code):
+        offeror_template_config = self.get_offeror_template_config()
+        template_conf = offeror_template_config.get(offeror_code)
+        if (
+            template_conf
+            and FieldChannel.objects.filter(
+                offeror_detail__offeror__code=offeror_code,
+                channel_type="offeror",
+            ).exists()
+        ):
+            channel = FieldChannel.objects.get(
+                offeror_detail__offeror__code=offeror_code,
+                channel_type="offeror",
+            )
+            channel.configuration.update(template_conf)
+            channel.save()
+
     def handle(self, *args, **options):  # noqa: ARG002
         """Create channels for topics, departments, offerors"""
         start = now_in_utc()
         pm = get_plugin_manager()
         hook = pm.hook
+
         overwrite = options["overwrite"]
         self.stdout.write(f"Overwriting existing channels?: {overwrite}")
         if options["all"] or options[ChannelType.department.name]:
@@ -67,6 +97,8 @@ class Command(BaseCommand):
             for offeror in LearningResourceOfferor.objects.all():
                 if hook.offeror_upserted(offeror=offeror, overwrite=overwrite)[0][1]:
                     created += 1
+                    self.set_default_offeror_template(offeror.code)
+
             self.stdout.write(f"Finished creating channels for {created} offerors")
         if options["all"] or options[ChannelType.topic.name]:
             created = 0
