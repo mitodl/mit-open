4447a0b516e62eefa561864a70fa4c96767602db
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-06-10 23:39:36 +0200

    Supply base API URL to tests

--- frontends/mit-open/src/common/urls.test.ts
+++ frontends/mit-open/src/common/urls.test.ts
@@ -3,19 +3,23 @@ import { login } from "./urls"
 const { axios_base_path: API_BASE_URL } = APP_SETTINGS
 
 test("login encodes the next parameter appropriately", () => {
-  expect(login()).toBe(`${API_BASE_URL}/login/ol-oidc/?next=/`)
-  expect(login({})).toBe(`${API_BASE_URL}/login/ol-oidc/?next=/`)
+  expect(login()).toBe(`${API_BASE_URL}/login/ol-oidc/?next=http://localhost/`)
+  expect(login({})).toBe(
+    `${API_BASE_URL}/login/ol-oidc/?next=http://localhost/`,
+  )
 
   expect(
     login({
       pathname: "/foo/bar",
     }),
-  ).toBe(`${API_BASE_URL}/login/ol-oidc/?next=/foo/bar`)
+  ).toBe(`${API_BASE_URL}/login/ol-oidc/?next=http://localhost/foo/bar`)
 
   expect(
     login({
       pathname: "/foo/bar",
       search: "?cat=meow",
     }),
-  ).toBe(`${API_BASE_URL}/login/ol-oidc/?next=/foo/bar%3Fcat%3Dmeow`)
+  ).toBe(
+    `${API_BASE_URL}/login/ol-oidc/?next=http://localhost/foo/bar%3Fcat%3Dmeow`,
+  )
 })
