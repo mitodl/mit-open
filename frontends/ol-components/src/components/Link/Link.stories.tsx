ccc997667a5d01c95d16132f0930a758c85608c8
Carey Gumaer <gumaerc@mit.edu>
2024-06-14 16:17:03 -0400

    change stories link color back to black

--- frontends/ol-components/src/components/Link/Link.stories.tsx
+++ frontends/ol-components/src/components/Link/Link.stories.tsx
@@ -24,7 +24,7 @@ const SIZES = [
   { size: "large", label: "Large" },
 ] as const
 const COLORS = [
-  { color: "darkGray2", label: "Black" },
+  { color: "black", label: "Black" },
   { color: "red", label: "Red" },
 ] as const
 
