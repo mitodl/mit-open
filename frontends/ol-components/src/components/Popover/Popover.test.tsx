5622fffc548dae6a3a8a4298bf8d382713558451
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-06-17 15:40:32 -0400

    Add popover component

---
+++ frontends/ol-components/src/components/Popover/Popover.test.tsx
@@ -0,0 +1,130 @@
+import React from "react"
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react"
+import user from "@testing-library/user-event"
+import { Popover } from "./Popover"
+import type { PopoverProps } from "./Popover"
+import { ThemeProvider } from "../ThemeProvider/ThemeProvider"
+
+const PopoverTest = (props: Omit<PopoverProps, "anchorEl">) => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
+  return (
+    <div>
+      <button ref={setAnchorEl}>Anchor</button>
+      {anchorEl && (
+        <Popover data-testid="popover" {...props} anchorEl={anchorEl}>
+          <h2>Popover content</h2>
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </Popover>
+      )}
+
+      <button>Other Button</button>
+    </div>
+  )
+}
+
+test.each([{ modal: true }, {}, {}])(
+  "Traps focus if modal = $modal",
+  async ({ modal }) => {
+    render(<PopoverTest open onClose={jest.fn()} modal={modal} />, {
+      wrapper: ThemeProvider,
+    })
+
+    const popover = screen.getByTestId("popover")
+    expect(popover.contains(document.activeElement)).toBe(true)
+
+    await user.tab()
+    expect(document.activeElement).toBe(
+      screen.getByRole("button", { name: "Button 1" }),
+    )
+
+    await user.tab()
+    expect(document.activeElement).toBe(
+      screen.getByRole("button", { name: "Button 2" }),
+    )
+
+    await user.tab()
+    expect(document.activeElement).toBe(
+      screen.getByRole("button", { name: "Button 1" }),
+    )
+  },
+)
+
+test("Popover does not trap focus if modal=false", async () => {
+  render(<PopoverTest open onClose={jest.fn()} modal={false} />, {
+    wrapper: ThemeProvider,
+  })
+
+  expect(document.activeElement).toBe(document.body)
+  await user.tab()
+  expect(document.activeElement).toBe(
+    screen.getByRole("button", { name: "Anchor" }),
+  )
+
+  await user.tab()
+  expect(document.activeElement).toBe(
+    screen.getByRole("button", { name: "Button 1" }),
+  )
+  await user.tab()
+  expect(document.activeElement).toBe(
+    screen.getByRole("button", { name: "Button 2" }),
+  )
+  await user.tab()
+  expect(document.activeElement).toBe(
+    screen.getByRole("button", { name: "Other Button" }),
+  )
+})
+
+test.each([
+  { role: "dialog", modal: true },
+  { role: "dialog" },
+  { role: "tooltip", modal: false },
+])("popover role is $role if $modal", ({ modal, role }) => {
+  render(<PopoverTest open onClose={jest.fn()} modal={modal} />, {
+    wrapper: ThemeProvider,
+  })
+  const popover = screen.getByTestId("popover")
+  const el = screen.getByRole(role)
+  expect(popover).toBe(el)
+})
+
+test("Calls onClose when escape is pressed", async () => {
+  const onClose = jest.fn()
+  render(<PopoverTest open onClose={onClose} />, {
+    wrapper: ThemeProvider,
+  })
+
+  await user.type(document.body, "{esc}")
+  expect(onClose).toHaveBeenCalled()
+})
+
+test("Calls onClose when clicking outside popover", async () => {
+  const onClose = jest.fn()
+  render(<PopoverTest open onClose={onClose} />, {
+    wrapper: ThemeProvider,
+  })
+
+  const popover = screen.getByTestId("popover")
+  expect(popover).toBeVisible()
+
+  await user.click(document.body)
+  expect(onClose).toHaveBeenCalled()
+})
+
+test("Popover is open/closed based on 'open'", async () => {
+  const onClose = jest.fn()
+  const { rerender } = render(<PopoverTest open onClose={onClose} />, {
+    wrapper: ThemeProvider,
+  })
+
+  const popover = screen.getByTestId("popover")
+  expect(popover).toBeVisible()
+
+  rerender(<PopoverTest open={false} onClose={onClose} />)
+
+  await waitForElementToBeRemoved(popover)
+})
---
+++ frontends/ol-components/src/components/Popover/Popover.tsx
@@ -0,0 +1,198 @@
+import * as React from "react"
+import styled from "@emotion/styled"
+import MuiPopper from "@mui/material/Popper"
+import type { PopperProps } from "@mui/material/Popper"
+import Fade from "@mui/material/Fade"
+import { FocusTrap } from "@mui/base/FocusTrap"
+import ClickAwayListener from "@mui/material/ClickAwayListener"
+
+const StyledPopper = styled(MuiPopper)(({ theme }) => ({
+  zIndex: 1,
+  "& > div": {
+    position: "relative",
+  },
+  '&[data-popper-placement*="bottom"]': {
+    "& > div": {
+      marginTop: 2,
+    },
+    "& .MuiPopper-arrow": {
+      top: 0,
+      marginTop: "-0.9em",
+      width: "3em",
+      height: "1em",
+      "&::before": {
+        borderWidth: "0 1em 1em 1em",
+        borderColor: `transparent transparent ${theme.palette.background.paper} transparent`,
+      },
+    },
+  },
+  '&[data-popper-placement*="top"]': {
+    "& > div": {
+      marginBottom: 2,
+    },
+    "& .MuiPopper-arrow": {
+      bottom: 0,
+      marginBottom: "-0.9em",
+      width: "3em",
+      height: "1em",
+      "&::before": {
+        borderWidth: "1em 1em 0 1em",
+        borderColor: `${theme.palette.background.paper} transparent transparent transparent`,
+      },
+    },
+  },
+  '&[data-popper-placement*="right"]': {
+    "& > div": {
+      marginLeft: 2,
+    },
+    "& .MuiPopper-arrow": {
+      left: 0,
+      marginLeft: "-0.9em",
+      height: "3em",
+      width: "1em",
+      "&::before": {
+        borderWidth: "1em 1em 1em 0",
+        borderColor: `transparent ${theme.palette.background.paper} transparent transparent`,
+      },
+    },
+  },
+  '&[data-popper-placement*="left"]': {
+    "& > div": {
+      marginRight: 2,
+    },
+    "& .MuiPopper-arrow": {
+      right: 0,
+      marginRight: "-0.9em",
+      height: "3em",
+      width: "1em",
+      "&::before": {
+        borderWidth: "1em 0 1em 1em",
+        borderColor: `transparent transparent transparent ${theme.palette.background.paper}`,
+      },
+    },
+  },
+}))
+
+const getModifiers = (
+  arrowEl: HTMLElement | null,
+): PopperProps["modifiers"] => [
+  {
+    name: "flip",
+    enabled: true,
+  },
+  {
+    name: "preventOverflow",
+    enabled: true,
+  },
+  {
+    name: "arrow",
+    enabled: !!arrowEl,
+    options: {
+      element: arrowEl,
+    },
+  },
+  {
+    name: "offset",
+    options: {
+      offset: [0, 10],
+    },
+  },
+  {
+    name: "preventOverflow",
+    enabled: true,
+    options: {
+      altAxis: true,
+      altBoundary: true,
+      tether: true,
+      padding: 8,
+    },
+  },
+]
+
+const Arrow = styled("div")({
+  position: "absolute",
+  fontSize: 7,
+  width: "3em",
+  height: "3em",
+  "&::before": {
+    content: '""',
+    margin: "auto",
+    display: "block",
+    width: 0,
+    height: 0,
+    borderStyle: "solid",
+  },
+})
+
+const Content = styled.div(({ theme }) => ({
+  padding: "16px",
+  backgroundColor: theme.custom.colors.white,
+  boxShadow:
+    "0px 2px 4px 0px rgba(37, 38, 43, 0.10), 0px 6px 24px 0px rgba(37, 38, 43, 0.24)",
+}))
+
+type PopoverProps = Pick<PopperProps, "anchorEl" | "placement" | "open"> & {
+  children?: React.ReactNode
+  /**
+   * Called when
+   *  - clicking outside the popover
+   *  - pressing "Escape"
+   */
+  onClose: () => void
+  /**
+   * If true (default: `true`), traps focus.
+   *
+   * Note: "Escape" closes the popover.
+   */
+  modal?: boolean
+}
+const Popover: React.FC<PopoverProps> = ({
+  children,
+  onClose,
+  modal = true,
+  open,
+  ...props
+}) => {
+  const [arrowRef, setArrowRef] = React.useState<HTMLDivElement | null>(null)
+  const modifiers = getModifiers(arrowRef)
+  const content = (
+    <div tabIndex={-1}>
+      <ClickAwayListener onClickAway={onClose}>
+        <Content>
+          <Arrow ref={setArrowRef} className="MuiPopper-arrow" />
+          {children}
+        </Content>
+      </ClickAwayListener>
+    </div>
+  )
+  return (
+    <StyledPopper
+      open={open}
+      modifiers={modifiers}
+      disablePortal
+      transition
+      {...props}
+      {...(modal ? { role: "dialog" } : {})}
+      onKeyDown={(event) => {
+        if (event.key === "Escape") {
+          onClose()
+        }
+      }}
+    >
+      {({ TransitionProps }) => (
+        <Fade {...TransitionProps} timeout={300}>
+          {modal ? (
+            <div>
+              <FocusTrap open={open}>{content}</FocusTrap>
+            </div>
+          ) : (
+            content
+          )}
+        </Fade>
+      )}
+    </StyledPopper>
+  )
+}
+
+export { Popover }
+export type { PopoverProps }
