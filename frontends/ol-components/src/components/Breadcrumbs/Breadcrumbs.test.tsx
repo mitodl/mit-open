a4df055882b9c2411f51f25f8344fcc0bd8213b3
Carey Gumaer <gumaerc@mit.edu>
2024-06-17 12:09:41 -0400

    need to mock Link

--- frontends/ol-components/src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ frontends/ol-components/src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -3,6 +3,13 @@ import { render, screen } from "@testing-library/react"
 import { ThemeProvider } from "../ThemeProvider/ThemeProvider"
 import { Breadcrumbs } from "./Breadcrumbs"
 
+// Mock react-router-dom's Link so we don't need to set up a Router
+jest.mock("react-router-dom", () => {
+  return {
+    Link: jest.fn((props) => <a href={props.to}>{props.children}</a>),
+  }
+})
+
 describe("Breadcrumbs", () => {
   test.each([
     { ancestors: [{ href: "/home", label: "Home" }] },
@@ -40,6 +47,7 @@ describe("Breadcrumbs", () => {
       expect(screen.getAllByRole("link")).toHaveLength(totalAncestors)
       expectedLabels.forEach((label, index) => {
         const link = screen.getByText(label)
+        expect(link).toBeInTheDocument()
         expect(link).toHaveAttribute("href", expectedHrefs[index])
       })
     },
