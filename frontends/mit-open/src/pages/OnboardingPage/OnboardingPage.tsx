8e6a79108982c725d349d007ad3048cce2ac0ed3
Nathan Levesque <nlevesq@mit.edu>
2024-06-28 16:29:39 -0400

    Switch to formik

--- frontends/mit-open/src/pages/OnboardingPage/OnboardingPage.tsx
+++ frontends/mit-open/src/pages/OnboardingPage/OnboardingPage.tsx
@@ -1,7 +1,14 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
-import isEqual from "lodash/isEqual"
 import range from "lodash/range"
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FieldProps,
+  FormikBag,
+} from "formik"
 import {
   styled,
   Step,
@@ -26,8 +33,6 @@ import { EducationLevelSelect } from "@/page-components/Profile/EducationLevelCh
 import { CertificateChoiceBoxField } from "@/page-components/Profile/CertificateChoice"
 import { useProfileMeMutation, useProfileMeQuery } from "api/hooks/profile"
 import { DASHBOARD } from "@/common/urls"
-
-import type { ProfileFieldUpdateFunc } from "@/page-components/Profile/types"
 
 const NUM_STEPS = 6
 
@@ -113,45 +118,32 @@ const Label = styled.div({
 })
 
 const OnboardingPage: React.FC = () => {
-  const [updates, setUpdates] = React.useState<PatchedProfileRequest>({})
   const { data: profile, isLoading: isLoadingProfile } = useProfileMeQuery()
   const { isLoading: isSaving, mutateAsync } = useProfileMeMutation()
   const [activeStep, setActiveStep] = React.useState<number>(0)
+  const isLastStep = activeStep == NUM_STEPS - 1
   const navigate = useNavigate()
 
-  const handleNext = () => {
+  const onSubmit = (values: PatchedProfileRequest, actions: FormikBag) => {
     // TODO: handle this error
-    mutateAsync(updates).then(() => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1)
-    })
-  }
-  const handleFinish = () => {
-    mutateAsync(updates).then(() => {
-      navigate(DASHBOARD)
+    mutateAsync(values).then(() => {
+      if (isLastStep) {
+        navigate(DASHBOARD)
+      } else {
+        actions.setValues({})
+        handleNext()
+      }
     })
   }
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1)
-  }
+  const handleBack = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  const handleNext = () => setActiveStep((prevActiveStep) => prevActiveStep + 1)
 
-  const handleUpdate: ProfileFieldUpdateFunc = <
-    T extends keyof PatchedProfileRequest,
-  >(
-    name: T,
-    value: PatchedProfileRequest[T],
-  ) => {
-    if (!isEqual(updates[name], value)) {
-      setUpdates({
-        [name]: value,
-      })
-    }
-  }
   if (typeof profile === "undefined") {
     return null
   }
 
-  return activeStep < NUM_STEPS ? (
+  return (
     <FlexContainer>
       <MetaTags title="Onboarding" />
       <StepContainer>
@@ -174,104 +166,101 @@ const OnboardingPage: React.FC = () => {
       {isLoadingProfile ? (
         <LoadingSpinner loading={true} />
       ) : (
-        <>
-          {activeStep === 0 ? (
-            <Container maxWidth="lg">
-              <TopicInterestsChoiceBoxField
-                label={
-                  <Label>
-                    <Title component="h3" variant="h6">
-                      Welcome{profile.name ? `, ${profile.name}` : ""}! What are
-                      you interested in learning about?
-                    </Title>
-                    <Prompt component="p">Select all that apply:</Prompt>
-                  </Label>
-                }
-                value={profile.topic_interests}
-                onUpdate={handleUpdate}
-              />
-            </Container>
-          ) : null}
-          {activeStep === 1 ? (
-            <Container maxWidth="lg">
-              <GoalsChoiceBoxField
-                label={
-                  <Label>
-                    <Title component="h3" variant="h6">
-                      What do you want MIT online education to help you reach?
-                    </Title>
-                    <Prompt component="p">Select all that apply:</Prompt>
-                  </Label>
-                }
-                value={profile.goals}
-                onUpdate={handleUpdate}
-              />
-            </Container>
-          ) : null}
-          {activeStep === 2 ? (
-            <Container maxWidth="md">
-              <CertificateChoiceBoxField
-                label={
-                  <Label>
-                    <Title component="h3" variant="h6">
-                      Are you seeking to receive a certificate?
-                    </Title>
-                    <Prompt>Select one:</Prompt>
-                  </Label>
-                }
-                value={profile.certificate_desired}
-                onUpdate={handleUpdate}
-              />
-            </Container>
-          ) : null}
-          {activeStep === 3 ? (
-            <Container maxWidth="sm">
-              <EducationLevelSelect
-                label={
-                  <Label>
-                    <Title component="h3" variant="h6">
-                      What is your current level of education?
-                    </Title>
-                  </Label>
-                }
-                value={profile.current_education}
-                onUpdate={handleUpdate}
-              />
-            </Container>
-          ) : null}
-          {activeStep === 4 ? (
-            <Container maxWidth="md">
-              <TimeCommitmentRadioChoiceBoxField
-                label={
-                  <Label>
-                    <Title component="h3" variant="h6">
-                      How much time per week do you want to commit to learning?
-                    </Title>
-                    <Prompt>Select one:</Prompt>
-                  </Label>
-                }
-                value={profile.time_commitment}
-                onUpdate={handleUpdate}
-              />
-            </Container>
-          ) : null}
-          {activeStep === 5 ? (
-            <Container maxWidth="md">
-              <LearningFormatChoiceBoxField
-                label={
-                  <Label>
-                    <Title component="h3" variant="h6">
-                      What course format are you interested in?
-                    </Title>
-                    <Prompt>Select one:</Prompt>
-                  </Label>
-                }
-                value={profile.learning_format}
-                onUpdate={handleUpdate}
-              />
-            </Container>
-          ) : null}
-        </>
+        <Formik onSubmit={onSubmit} initialValues={}>
+          <Form>
+            {activeStep === 0 ? (
+              <Container maxWidth="lg">
+                <TopicInterestsChoiceBoxField
+                  label={
+                    <Label>
+                      <Title component="h3" variant="h6">
+                        Welcome{profile.name ? `, ${profile.name}` : ""}! What
+                        are you interested in learning about?
+                      </Title>
+                      <Prompt component="p">Select all that apply:</Prompt>
+                    </Label>
+                  }
+                  value={profile.topic_interests}
+                />
+              </Container>
+            ) : null}
+            {activeStep === 1 ? (
+              <Container maxWidth="lg">
+                <GoalsChoiceBoxField
+                  label={
+                    <Label>
+                      <Title component="h3" variant="h6">
+                        What do you want MIT online education to help you reach?
+                      </Title>
+                      <Prompt component="p">Select all that apply:</Prompt>
+                    </Label>
+                  }
+                  value={profile.goals}
+                />
+              </Container>
+            ) : null}
+            {activeStep === 2 ? (
+              <Container maxWidth="md">
+                <CertificateChoiceBoxField
+                  label={
+                    <Label>
+                      <Title component="h3" variant="h6">
+                        Are you seeking to receive a certificate?
+                      </Title>
+                      <Prompt>Select one:</Prompt>
+                    </Label>
+                  }
+                  value={profile.certificate_desired}
+                />
+              </Container>
+            ) : null}
+            {activeStep === 3 ? (
+              <Container maxWidth="sm">
+                <EducationLevelSelect
+                  label={
+                    <Label>
+                      <Title component="h3" variant="h6">
+                        What is your current level of education?
+                      </Title>
+                    </Label>
+                  }
+                  value={profile.current_education}
+                />
+              </Container>
+            ) : null}
+            {activeStep === 4 ? (
+              <Container maxWidth="md">
+                <TimeCommitmentRadioChoiceBoxField
+                  label={
+                    <Label>
+                      <Title component="h3" variant="h6">
+                        How much time per week do you want to commit to
+                        learning?
+                      </Title>
+                      <Prompt>Select one:</Prompt>
+                    </Label>
+                  }
+                  value={profile.time_commitment}
+                />
+              </Container>
+            ) : null}
+            {activeStep === 5 ? (
+              <Container maxWidth="md">
+                <LearningFormatChoiceBoxField
+                  label={
+                    <Label>
+                      <Title component="h3" variant="h6">
+                        What course format are you interested in?
+                      </Title>
+                      <Prompt>Select one:</Prompt>
+                    </Label>
+                  }
+                  value={profile.learning_format}
+                />
+              </Container>
+            ) : null}
+          </Form>
+        </Formik>
       )}
       <NavControls>
         {activeStep > 0 ? (
@@ -284,26 +273,16 @@ const OnboardingPage: React.FC = () => {
             Back
           </Button>
         ) : null}
-        {activeStep < NUM_STEPS - 1 ? (
-          <Button
-            endIcon={isSaving ? <CircularProgress /> : <RiArrowRightLine />}
-            onClick={handleNext}
-            disabled={isSaving}
-          >
-            Next
-          </Button>
-        ) : (
-          <Button
-            endIcon={isSaving ? <CircularProgress /> : <RiArrowRightLine />}
-            onClick={handleFinish}
-            disabled={isSaving}
-          >
-            Finish
-          </Button>
-        )}
+        <Button
+          endIcon={isSaving ? <CircularProgress /> : <RiArrowRightLine />}
+          disabled={isSaving}
+          type="submit"
+        >
+          {isLastStep ? "Finish" : "Next"}
+        </Button>
       </NavControls>
     </FlexContainer>
-  ) : null
+  )
 }
 
 export default OnboardingPage
