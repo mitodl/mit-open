fc641b3326b3497bfdca22c0216e744ee5436b6b
Matt Bertrand <mrbertrand@gmail.com>
2024-07-01 16:51:21 -0400

    Tweak new task

--- learning_resources/tasks.py
+++ learning_resources/tasks.py
@@ -70,7 +70,8 @@ def get_oll_data():
 @app.task
 def get_professional_ed_data():
     """Execute the Professional Education ETL pipeline"""
-    return len(pipelines.professional_ed_etl())
+    courses, programs = pipelines.professional_ed_etl()
+    return len(courses + programs)
 
 
 @app.task
