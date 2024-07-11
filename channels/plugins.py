c26045e48d74d4bfcb3ca54fdd7ad13c54121c2c
Carey Gumaer <gumaerc@mit.edu>
2024-07-11 14:15:33 -0400

    change department_upserted hook to match its update_or_create on search_filter instead of name because name can change, and search_filter should stay the same since it's based on department_id

--- channels/plugins.py
+++ channels/plugins.py
@@ -73,11 +73,12 @@ class ChannelPlugin:
             channel = None
         elif department.school and (overwrite or not channel):
             channel, _ = Channel.objects.update_or_create(
+                search_filter=f"department={department.department_id}",
                 name=slugify(department.name),
                 channel_type=ChannelType.department.name,
                 defaults={
+                    "name": slugify(department.name),
                     "title": department.name,
-                    "search_filter": f"department={department.department_id}",
                 },
             )
             ChannelDepartmentDetail.objects.update_or_create(
