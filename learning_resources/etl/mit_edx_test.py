286596b0c6aa8591cba15bc08c5542b58aa29c59
James Kachel <jkachel@mit.edu>
2024-07-22 14:54:16 -0500

    Updating edx pipelines to use the topic mappings

--- learning_resources/etl/mit_edx_test.py
+++ learning_resources/etl/mit_edx_test.py
@@ -2,7 +2,16 @@
 
 import copy
 
-from learning_resources.etl.mit_edx import EDX_TOPIC_MAPPINGS, transform
+import pytest
+
+from learning_resources.constants import OfferedBy
+from learning_resources.etl.mit_edx import transform
+from learning_resources.models import (
+    LearningResourceOfferor,
+    LearningResourceTopicMapping,
+)
+
+pytestmark = pytest.mark.django_db
 
 
 def test_mitx_transform_non_mit_owner(non_mitx_course_data):
@@ -17,20 +26,19 @@ def test_mitx_transform_mit_owner(mitx_course_data):
 
 def test_mitx_transform_remap_topics(mocker, mitx_course_data):
     """Verify that course topics are remapped correctly"""
-    mock_log = mocker.patch("learning_resources.etl.mit_edx.log", autospec=True)
+    mapped_topics = [
+        (topic.topic_name, topic.topic.name)
+        for topic in LearningResourceTopicMapping.objects.filter(
+            offeror=LearningResourceOfferor.objects.filter(
+                code=OfferedBy.mitx.name
+            ).first()
+        ).all()
+    ]
 
-    for edx_topic, expected_topic in EDX_TOPIC_MAPPINGS.items():
-        mock_log.reset_mock()
-
+    for edx_topic, expected_topic in mapped_topics:
         data = copy.deepcopy(mitx_course_data["results"])
         data[0]["subjects"] = [{"name": edx_topic}, {"name": "this topic isn't mapped"}]
 
         course = next(transform(data))
-
-        mock_log.info.assert_called_once_with(
-            "Failed to map mitx topic '%s' for course '%s'",
-            "this topic isn't mapped",
-            course["readable_id"],
-        )
 
         assert course["topics"] == [{"name": expected_topic}]
