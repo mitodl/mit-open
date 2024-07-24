3dd3b4476021da55e8ff4e78ceb6a8cd8a03203d
James Kachel <jkachel@mit.edu>
2024-07-24 10:52:25 -0500

    Reorder where the testimonial displays in the unit/offeror page to fix spacing and background

--- frontends/mit-open/src/pages/ChannelPage/UnitChannelTemplate.tsx
+++ frontends/mit-open/src/pages/ChannelPage/UnitChannelTemplate.tsx
@@ -21,6 +21,7 @@ import { getSearchParamMap } from "@/common/utils"
 import { HOME as HOME_URL, UNITS as UNITS_URL } from "../../common/urls"
 import { ChannelTypeEnum } from "api/v0"
 import { ChannelControls, UNITS_LABEL } from "./ChannelPageTemplate"
+import TestimonialDisplay from "@/page-components/TestimonialDisplay/TestimonialDisplay"
 
 const StyledBannerBackground = styled(BannerBackground)(({ theme }) => ({
   padding: "48px 0 64px 0",
@@ -182,8 +183,9 @@ const UnitChannelTemplate: React.FC<UnitChannelTemplateProps> = ({
           config={FEATURED_RESOURCES_CAROUSEL}
           isLoading={channel.isLoading}
         />
-        {children}
       </Container>
+      <TestimonialDisplay offerors={[name]} />
+      <Container>{children}</Container>
     </>
   )
 }
