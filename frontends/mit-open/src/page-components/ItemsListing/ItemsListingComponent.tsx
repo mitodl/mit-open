627e194b265dd940708cb921867bead77af50207
Carey Gumaer <gumaerc@mit.edu>
2024-08-02 16:34:41 -0400

    restyle dashboard user list view component

--- frontends/mit-open/src/page-components/ItemsListing/ItemsListingComponent.tsx
+++ frontends/mit-open/src/page-components/ItemsListing/ItemsListingComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { Grid, Button, Typography } from "ol-components"
-import { RiPencilFill, RiArrowUpDownLine } from "@remixicon/react"
+import { Grid, Button, Typography, styled } from "ol-components"
+import { RiArrowUpDownLine } from "@remixicon/react"
 import { useToggle, pluralize } from "ol-utilities"
 import { GridColumn, GridContainer } from "@/components/GridLayout/GridLayout"
 import ItemsListing from "./ItemsListing"
@@ -25,6 +25,38 @@ type ItemsListingComponentProps = {
   condensed?: boolean
 }
 
+const DescriptionText = styled(Typography)(({ theme }) => ({
+  color: theme.custom.colors.silverGrayDark,
+  ...theme.typography.body1,
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}))
+
+const HeaderGrid = styled(Grid)(({ theme }) => ({
+  gap: "24px",
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+  },
+}))
+
+const EditButton = styled(Button)(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+  },
+}))
+
+const ReorderGrid = styled(Grid)(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}))
+
+const CountText = styled(Typography)(({ theme }) => ({
+  color: theme.custom.colors.silverGrayDark,
+  ...theme.typography.buttonSmall,
+}))
+
 const ItemsListingComponent: React.FC<ItemsListingComponentProps> = ({
   listType,
   list,
@@ -44,51 +76,53 @@ const ItemsListingComponent: React.FC<ItemsListingComponentProps> = ({
     <GridContainer>
       <GridColumn variant="single-full">
         <Grid container>
-          <Grid item xs={12}>
-            <Typography variant="h3" component="h1">
-              {list?.title}
-            </Typography>
-            {list?.description && <p>{list.description}</p>}
-          </Grid>
           <Grid
             item
-            xs={6}
+            container
+            alignItems="center"
+            justifyContent="space-between"
+            marginBottom="24px"
+          >
+            <HeaderGrid item>
+              <Typography variant="h3" component="h1">
+                {list?.title}
+              </Typography>
+              {list?.description && (
+                <DescriptionText>{list.description}</DescriptionText>
+              )}
+            </HeaderGrid>
+            <HeaderGrid item alignSelf="flex-start">
+              {canEdit ? (
+                <EditButton variant="primary" onClick={handleEdit}>
+                  Edit List
+                </EditButton>
+              ) : null}
+            </HeaderGrid>
+          </Grid>
+          <ReorderGrid
+            item
             container
             alignItems="center"
             justifyContent="space-between"
           >
-            {showSort && !!items.length && (
-              <Button
-                variant="text"
-                disabled={count === 0}
-                startIcon={isSorting ? undefined : <RiArrowUpDownLine />}
-                onClick={toggleIsSorting.toggle}
-              >
-                {isSorting ? "Done ordering" : "Reorder"}
-              </Button>
-            )}
-            {count !== undefined && count > 0
-              ? `${count} ${pluralize("item", count)}`
-              : null}
-          </Grid>
-          <Grid
-            item
-            xs={6}
-            container
-            justifyContent="flex-end"
-            alignItems="center"
-            display="flex"
-          >
-            {canEdit ? (
-              <Button
-                variant="text"
-                startIcon={<RiPencilFill />}
-                onClick={handleEdit}
-              >
-                Edit
-              </Button>
-            ) : null}
-          </Grid>
+            <Grid item>
+              {showSort && !!items.length && (
+                <Button
+                  variant="text"
+                  disabled={count === 0}
+                  startIcon={isSorting ? undefined : <RiArrowUpDownLine />}
+                  onClick={toggleIsSorting.toggle}
+                >
+                  {isSorting ? "Done ordering" : "Reorder"}
+                </Button>
+              )}
+            </Grid>
+            <Grid item>
+              {count !== undefined && count > 0 ? (
+                <CountText>{`${count} ${pluralize("item", count)}`}</CountText>
+              ) : null}
+            </Grid>
+          </ReorderGrid>
         </Grid>
         <ItemsListing
           listType={listType}
