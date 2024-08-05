64cdc754f1a6651b9622fc2c670fa1dd79f4a373
Shankar Ambady <shanbady@gmail.com>
2024-08-05 16:29:57 -0400

    Subscription email template updates (#1311)
    * adding resource type
    
    * conditionalizing subject line
    
    * limiting list of resources to 5
    
    * adding images to email
    
    * fixing f-string
    
    * template style fixes
    
    * fixing logo
    
    * formatting changes and adding footer
    
    * adding url context to email templates and some style changes
    
    * adding test
    
    * fixing button float and adding background
    
    * cloning model method utilities
    
    * inferring channel urls for groups
    
    * initial changes to send individual emails
    
    * fixing view more url and checking for null in resource.image
    
    * fixing subject line
    
    * fixing footer urls
    
    * capping at 10 items
    
    * fixing saved search label
    
    * fixing saved search type
    
    * fix spacing
    
    * fixing tests
    
    * adding short subject in template and some style fixes
    
    * fixing method
    
    * updating dashboard settings url to match upcoming changes in pr #1348

--- learning_resources_search/models.py
+++ learning_resources_search/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models import JSONField
 
-from channels.constants import ChannelType
 from channels.models import Channel
 from main.models import TimestampedModel
 
@@ -29,11 +28,25 @@ class PercolateQuery(TimestampedModel):
     )
     users = models.ManyToManyField(User, related_name="percolate_queries")
 
-    def __str__(self):
-        return f"Percolate query {self.id}: {self.query}"
+    def source_label(self):
+        source_channel = self.source_channel()
+        if source_channel:
+            return source_channel.channel_type
+        else:
+            return "saved_search"
 
-    class Meta:
-        unique_together = (("source_type", "original_query"),)
+    def source_description(self):
+        channel = self.source_channel()
+        if channel:
+            return channel.title
+        return self.original_url_params()
+
+    def source_channel(self):
+        original_query_params = self.original_url_params()
+        channels_filtered = Channel.objects.filter(search_filter=original_query_params)
+        if channels_filtered.exists():
+            return channels_filtered.first()
+        return None
 
     def original_url_params(self):
         ignore_params = ["endpoint"]
@@ -43,19 +56,8 @@ class PercolateQuery(TimestampedModel):
         }
         return urlencode(defined_params, doseq=True)
 
-    def source_label(self):
-        original_query_params = self.original_url_params()
-        channels_filtered = Channel.objects.filter(search_filter=original_query_params)
-        if channels_filtered.exists():
-            return channels_filtered.first().channel_type
-        else:
-            return "saved_search"
+    def __str__(self):
+        return f"Percolate query {self.id}: {self.query}"
 
-    def source_description(self):
-        original_query_params = self.original_url_params()
-        source_label = self.source_label()
-
-        if source_label in ChannelType:
-            channel = Channel.objects.get(search_filter=original_query_params)
-            return channel.title
-        return self.original_url_params()
+    class Meta:
+        unique_together = (("source_type", "original_query"),)
