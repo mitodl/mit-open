36465bab2d01590b4643a2c2413720e88ab419c6
Carey Gumaer <gumaerc@mit.edu>
2024-08-09 15:16:37 -0400

    fix storybook globalstyles import path

--- frontends/mit-learn/.storybook/preview.tsx
+++ frontends/mit-learn/.storybook/preview.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { ThemeProvider } from "ol-components"
 
 import { Preview } from "@storybook/react"
-import GlobalStyles from "./GlobalStyles"
+import GlobalStyles from "../src/GlobalStyles"
 
 const preview: Preview = {
   decorators: [
