9d0b321edb0ab4d82b241abeb0eb7d781a1c2e2e
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-08-14 19:44:48 +0200

    Styled elements

---
+++ frontends-ssr/mit-learn/src/app/styled.tsx
@@ -0,0 +1,21 @@
+
+"use client"
+
+import { styled } from "ol-components"
+
+/*
+ * Use in server components gives:
+ * Error: Cannot access styled.div on the server. You cannot dot into a client module from a server component. You can only pass the imported name through.
+ * Solution for now is
+ */
+
+
+export const PageWrapper = styled.div({
+  height: "calc(100vh - 80px)",
+  display: "flex",
+  flexDirection: "column",
+})
+
+export const PageWrapperInner = styled.div({
+  flex: "1",
+})
