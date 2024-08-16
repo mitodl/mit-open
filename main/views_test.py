2fd772c024f2e696f7c492111c2a32a995115938
James Kachel <jkachel@mit.edu>
2024-08-16 14:19:57 -0500

    Fixing view tests

--- main/views_test.py
+++ main/views_test.py
@@ -3,16 +3,15 @@
 import uuid
 
 
-def test_anon_error(client, mocker):
+def test_anon_error(client):
     """Test that we get an error as we expect from a nonsense URL with an anonymous session."""
 
     response = client.get(f"/{uuid.uuid4()}")
 
-    # Silk tends to grab bad URLs and then complains because there's no credentials..
-    assert response.status_code == 403
+    assert response.status_code == 404
 
 
-def test_authed_error(user_client, mocker):
+def test_authed_error(user_client):
     """Test that we get an error as we expect from a nonsense URL with a session."""
 
     response = user_client.get(f"/{uuid.uuid4()}")
