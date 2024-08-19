db13c6e25d4ce23ffc0128fe6b19a4847f2d5bab
James Kachel <james@jkachel.com>
2024-08-19 18:33:11 -0500

    Remove remaining frontend routes (#1424)

--- main/views.py
+++ main/views.py
@@ -1,52 +1,35 @@
 """
-main views
+Base utility views. Handles errors and feature list views.
 """
 
-from django.http import (
-    HttpResponseBadRequest,
-    HttpResponseForbidden,
-    HttpResponseNotFound,
-)
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
 from main.features import get_all_feature_flags, is_enabled
 
 
-def index(request, **kwargs):  # pylint: disable=unused-argument  # noqa: ARG001
-    """
-    Return the static HTML index file for our react app.
-
-    In general, this should not be served by Django, but directly by nginx or
-    another web server. However, if a route that nginx sends to Django is a 404,
-    then we return the react app via Django.
-    """
-    return render(request, "index.html")
-
-
-def handle_400(
-    request,
+@api_view()
+@permission_classes([AllowAny])
+def handle_error(
+    request,  # noqa: ARG001
     exception=None,  # noqa: ARG001
 ):
-    """400 error handler"""
-    return HttpResponseBadRequest(index(request))
+    """Client Error"""
 
+    # This is a generic handler, since the api_view decorator means DRF will
+    # usurp error handling and provide whatever response is actually necessary.
+    # There's a 404 here just as a fallback.
 
-def handle_403(
-    request,
-    exception=None,  # noqa: ARG001
-):
-    """403 error handler"""
-    return HttpResponseForbidden(index(request))
-
-
-def handle_404(
-    request,
-    exception=None,  # noqa: ARG001
-):
-    """404 error handler"""
-    return HttpResponseNotFound(index(request))
+    return Response(
+        {
+            "detail": "The specified resource was not found.",
+            "error_type": "Http404",
+        },
+        status=status.HTTP_404_NOT_FOUND,
+    )
 
 
 class FeaturesViewSet(ViewSet):
