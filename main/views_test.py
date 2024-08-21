7b5086930bd059e85aa18375f9c83aba23a429e5
shankar ambady <ambady@mit.edu>
2024-08-21 10:59:36 -0400

    adding test for redirect view

--- main/views_test.py
+++ main/views_test.py
@@ -17,3 +17,13 @@ def test_authed_error(user_client):
     response = user_client.get(f"/{uuid.uuid4()}")
 
     assert response.status_code == 404
+
+
+def test_redirect_route(settings, user_client):
+    """
+    Simple Test that checks that we have a catch all redirect view
+    so that is not accidently removed
+    """
+    response = user_client.get("/app", follow=True)
+    assert response.redirect_chain[0][0] == settings.APP_BASE_URL
+    assert response.redirect_chain[0][1] == 302
