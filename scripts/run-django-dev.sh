d0f4fa73833da03f870f4ea4f71ae6276765de38
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-08-21 15:36:00 -0400

    fix fixture autoloading

--- scripts/run-django-dev.sh
+++ scripts/run-django-dev.sh
@@ -6,9 +6,7 @@ python3 manage.py collectstatic --noinput --clear
 python3 manage.py migrate --noinput
 RUN_DATA_MIGRATIONS=true python3 manage.py migrate --noinput
 
-if [[ $NODE_ENV == "development" ]]; then
-	# load required fixtures on development by default
-	python3 manage.py loaddata platforms schools departments offered_by
-fi
+# load required fixtures on development by default
+python3 manage.py loaddata platforms schools departments offered_by
 
 uwsgi uwsgi.ini --honour-stdin
