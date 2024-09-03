f3f5e7a7a0cdedfc86ea0e0eaa3f677e59320130
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-09-03 19:03:00 +0200

    Next.js Initial Migration (#1505)
    * Next.js scaffold
    
    * Supply env vars on process.env.NEXT_PUBLIC_
    
    * Replace react-router hook with next/navigation
    
    * MUI theme provider for Next.js
    
    * Styled elements
    
    * Restructuring to bring pages and components into ssr project
    
    * Images and link fixes
    
    * Set up prerendering with React Query and fetch Featured Courses carousel on server
    
    * Head meta tags
    
    * Main entrypoint. Include in yarn workspaces. API URL for local
    
    * Add remaining homepage sections
    
    * Move into frontends
    
    * Lint fixes
    
    * Routed drawer and updates for homepage resource drawer search triggers
    
    * Move pages (conflict with Next.js Pages Router)
    
    * Webpack configs for babel loader (import type) and ignore test files from build
    
    * Typescript config, move common/urls
    
    * Updates towards successful build
    
    * Suspense boudary for header useSearchParam
    
    * Provide origin on environment (cannot reference window)
    
    * Update drawer hooks for next/navigation
    
    * Unit listing page and dependencies
    
    * Image fixes. ol-component compatibility updates
    
    * Logo image fixes and footer image
    
    * Search page
    
    * Remove temp course-serach-utils
    
    * Remove temp course-serach-utils
    
    * Channel Page. Rewrite for image paths
    
    * Move @mui/material-nextjs into ol-components
    
    * Department listing page
    
    * Topic listing page
    
    * Terms page
    
    * Privacy page
    
    * Image paths
    
    * Lockfile
    
    * Migrates the dashboard pages
    
    * Migrate learning path listing page
    
    * Migrate learning path details page
    
    * Prettier fixes
    
    * Migrate test utils. Fixes for passing typechecks on test files (excluding React Router dependencies)
    
    * Migrate the onboarding page
    
    * Migrate program letter page
    
    * Migrate error page
    
    * Resolves issue with Server Components treated as Client Components (transpiler). Wraps library imports for use in Next.js. User lists detail page.
    
    * Metadata utility
    
    * Fetch learning resource for metadata when drawer is open
    
    * Async and sync metadata utilities
    
    * .env example file
    
    * Preserve hero image aspect
    
    * Put back SliderInput

--- frontends/ol-utilities/src/index.ts
+++ frontends/ol-utilities/src/index.ts
@@ -1,7 +1,8 @@
+"use client"
+
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./types/settings.d.ts" />
 
-export * from "./components"
 export * from "./styles"
 
 export * from "./date/format"
