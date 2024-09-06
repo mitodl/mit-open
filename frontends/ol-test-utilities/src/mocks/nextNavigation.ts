1e5c5798ea6f67d812735ebc88aa820292112ce1
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-09-06 16:31:37 -0400

    fix remaining nextjs tests in ol-components

---
+++ frontends/ol-test-utilities/src/mocks/nextNavigation.ts
@@ -0,0 +1,26 @@
+/**
+ * This is a mock for the next/navigation module based on the next-router-mock
+ *
+ *
+ */
+import * as mocks from "next-router-mock"
+
+export const nextNavigationMocks = {
+  ...mocks,
+  notFound: jest.fn(),
+  redirect: jest.fn().mockImplementation((url: string) => {
+    nextNavigationMocks.memoryRouter.setCurrentUrl(url)
+  }),
+  usePathname: () => {
+    const router = nextNavigationMocks.useRouter()
+    return router.asPath
+  },
+  useSearchParams: () => {
+    const router = nextNavigationMocks.useRouter()
+    const path = router.query
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return new URLSearchParams(path as any)
+  },
+}
+const mockRouter = nextNavigationMocks.memoryRouter
+export { mockRouter }
