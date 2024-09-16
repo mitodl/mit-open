394df4a42d508c2d35a6d7addce69ea6cf47c451
shankar ambady <ambady@mit.edu>
2024-09-16 14:44:10 -0400

    using getMetadataAsync for open resource drawer

--- frontends/main/src/app/c/[channelType]/[name]/page.tsx
+++ frontends/main/src/app/c/[channelType]/[name]/page.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ChannelPage from "@/app-pages/ChannelPage/ChannelPage"
 import { channelsApi } from "api/clients"
 import { ChannelTypeEnum } from "api/v0"
-
+import { getMetadataAsync } from "@/common/metadata"
 const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
 
 type RouteParams = {
@@ -10,11 +10,24 @@ type RouteParams = {
   name: string
 }
 
-export async function generateMetadata({ params }: { params: RouteParams }) {
+export async function generateMetadata({
+  searchParams,
+  params,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+  params: RouteParams
+}) {
   const { channelType, name } = params
+
   const channelDetails = await channelsApi
     .channelsTypeRetrieve({ channel_type: channelType, name: name })
     .then((res) => res.data)
+  if (searchParams?.resource) {
+    return await getMetadataAsync({
+      title: "Learn with MIT",
+      searchParams,
+    })
+  }
   return {
     title: `${channelDetails.title} | ${SITE_NAME}`,
     description: channelDetails.public_description,
