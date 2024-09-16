cfd38aa6c0b9f2a31d757532e1890b8585e9dc22
shankar ambady <ambady@mit.edu>
2024-09-16 11:32:49 -0400

    adding metadata to channel details page

--- frontends/main/src/app/c/[channelType]/[name]/page.tsx
+++ frontends/main/src/app/c/[channelType]/[name]/page.tsx
@@ -1,5 +1,23 @@
 import React from "react"
 import ChannelPage from "@/app-pages/ChannelPage/ChannelPage"
+import { channelsApi } from "api/clients"
+
+import { GetStaticPropsContext } from "next"
+
+export async function generateMetadata({ params }: GetStaticPropsContext) {
+  const { channelType, name } = params
+  const channelDetails = await channelsApi
+    .channelsTypeRetrieve({ channel_type: channelType, name: name })
+    .then((res) => res.data)
+  return {
+    title: `${channelDetails.title} | MIT Learn`,
+    description: channelDetails.public_description,
+    openGraph: {
+      images: channelDetails.configuration.logo,
+      url: channelDetails.channel_url,
+    },
+  }
+}
 
 const Page: React.FC = () => {
   return <ChannelPage />
