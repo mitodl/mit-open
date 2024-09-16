a2dec43596b76f357036ec277953e3f2afd020c3
shankar ambady <ambady@mit.edu>
2024-09-16 11:58:14 -0400

    refactor

--- frontends/main/src/app/c/[channelType]/[name]/page.tsx
+++ frontends/main/src/app/c/[channelType]/[name]/page.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import ChannelPage from "@/app-pages/ChannelPage/ChannelPage"
 import { channelsApi } from "api/clients"
+import { ChannelTypeEnum } from "api/v0"
 
-export async function generateMetadata({ params }) {
+type RouteParams = {
+  channelType: ChannelTypeEnum
+  name: string
+}
+
+export async function generateMetadata({ params }: { params: RouteParams }) {
   const { channelType, name } = params
   const channelDetails = await channelsApi
     .channelsTypeRetrieve({ channel_type: channelType, name: name })
