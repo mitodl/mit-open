70385b2294522ee47a39e672f0c953a871ec9826
shankar ambady <ambady@mit.edu>
2024-09-16 15:25:07 -0400

    switching to getMetaData

--- frontends/main/src/app/learningpaths/page.tsx
+++ frontends/main/src/app/learningpaths/page.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import LearningPathListingPage from "@/app-pages/LearningPathListingPage/LearningPathListingPage"
+import { getMetadata } from "@/common/metadata"
 
 const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
 
-export const metadata = {
+export const metadata: Metadata = getMetadata({
   title: `Learning Paths | ${SITE_NAME}`,
-}
+})
 
 const Page: React.FC = () => {
   return <LearningPathListingPage />
