133fb8cd5a907799998e30f9a2d2177577151cda
shankar ambady <ambady@mit.edu>
2024-09-16 12:59:21 -0400

    adding site name

--- frontends/main/src/app/learningpaths/page.tsx
+++ frontends/main/src/app/learningpaths/page.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 import LearningPathListingPage from "@/app-pages/LearningPathListingPage/LearningPathListingPage"
 
+const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
+
+export const metadata = {
+  title: `Learning Paths | ${SITE_NAME}`,
+}
+
 const Page: React.FC = () => {
   return <LearningPathListingPage />
 }
