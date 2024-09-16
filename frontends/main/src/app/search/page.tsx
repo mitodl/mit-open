4e20d9a114fa071922c91084a132063190ede6b6
shankar ambady <ambady@mit.edu>
2024-09-16 16:09:01 -0400

    fixing search page title and edit channel details title

--- frontends/main/src/app/search/page.tsx
+++ frontends/main/src/app/search/page.tsx
@@ -1,11 +1,24 @@
 import React, { Suspense } from "react"
-import { Metadata } from "next"
-import { getMetadata } from "@/common/metadata"
+import { getMetadataAsync } from "@/common/metadata"
 import SearchPage from "@/app-pages/SearchPage/SearchPage"
+const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
 
-export const metadata: Metadata = getMetadata({
-  title: "Search",
-})
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  if (searchParams?.resource) {
+    return await getMetadataAsync({
+      title: "Learn with MIT",
+      searchParams,
+    })
+  }
+  return await getMetadataAsync({
+    title: `Search | ${SITE_NAME}`,
+    searchParams,
+  })
+}
 
 const Page: React.FC = () => {
   return (
