db15383efb5850f167e6ec776b6250d2f3ddf190
shankar ambady <ambady@mit.edu>
2024-09-16 16:30:13 -0400

    removing redundant metatag call

--- frontends/main/src/app/search/page.tsx
+++ frontends/main/src/app/search/page.tsx
@@ -1,21 +1,14 @@
 import React, { Suspense } from "react"
 import { getMetadataAsync } from "@/common/metadata"
 import SearchPage from "@/app-pages/SearchPage/SearchPage"
-const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
 
 export async function generateMetadata({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
-  if (searchParams?.resource) {
-    return await getMetadataAsync({
-      title: "Learn with MIT",
-      searchParams,
-    })
-  }
   return await getMetadataAsync({
-    title: `Search | ${SITE_NAME}`,
+    title: "Search",
     searchParams,
   })
 }
