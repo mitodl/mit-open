a0979a039b4d315b58a4569299518a7cbc96a548
shankar ambady <ambady@mit.edu>
2024-09-18 12:54:01 -0400

    adding site name

--- frontends/main/src/app/units/page.tsx
+++ frontends/main/src/app/units/page.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import { Metadata } from "next"
 import { getMetadata } from "@/common/metadata"
 import UnitsListingPage from "@/app-pages/UnitsListingPage/UnitsListingPage"
-
+const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME
 export const metadata: Metadata = getMetadata({
-  title: "Units | MIT Learn",
+  title: `Units | ${SITE_NAME}`,
 })
 
 const Page: React.FC = () => {
