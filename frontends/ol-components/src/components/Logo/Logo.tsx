addbaa4df15c344b31db8ce986165d1f3bfe0b28
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2024-09-26 20:42:04 +0200

    Specify dimensions for logos (constrain by width or height)

--- frontends/ol-components/src/components/Logo/Logo.tsx
+++ frontends/ol-components/src/components/Logo/Logo.tsx
@@ -1,99 +1,132 @@
 import React from "react"
 import { PlatformEnum } from "api"
+import Image from "next/image"
 
-type PlatformObject = {
+
+type WithImage = {
   name: string
-  image: string | null
+  image: string
+  aspect: number
 }
+
+type WithoutImage = {
+  name: string
+  image?: null
+}
+
+type PlatformObject = WithImage | WithoutImage
 
 export const PLATFORMS: Record<PlatformEnum, PlatformObject> = {
   [PlatformEnum.Ocw]: {
     name: "MIT OpenCourseWare",
     image: "/unit_logos/ocw.svg",
+    aspect: 6.03
   },
   [PlatformEnum.Edx]: {
     name: "edX",
     image: "/platform_logos/edx.svg",
+    aspect: 1.77
   },
   [PlatformEnum.Mitxonline]: {
     name: "MITx Online",
     image: "/unit_logos/mitx.svg",
+    aspect: 3.32
   },
   [PlatformEnum.Bootcamps]: {
     name: "Bootcamps",
     image: "/platform_logos/bootcamps.svg",
+    aspect: 5.25
   },
   [PlatformEnum.Xpro]: {
     name: "MIT xPRO",
     image: "/unit_logos/xpro.svg",
+    aspect: 3.56
   },
   [PlatformEnum.Podcast]: {
     name: "Podcast",
-    image: null,
   },
   [PlatformEnum.Csail]: {
     name: "CSAIL",
     image: "/platform_logos/csail.svg",
+    aspect: 1.76
   },
   [PlatformEnum.Mitpe]: {
     name: "MIT Professional Education",
-    image: null,
   },
   [PlatformEnum.See]: {
     name: "MIT Sloan Executive Education",
     image: "/unit_logos/see.svg",
+    aspect: 7.73
   },
   [PlatformEnum.Scc]: {
     name: "Schwarzman College of Computing",
-    image: null,
   },
   [PlatformEnum.Ctl]: {
     name: "Center for Transportation & Logistics",
-    image: null,
   },
   [PlatformEnum.Emeritus]: {
     name: "Emeritus",
-    image: null,
   },
   [PlatformEnum.Simplilearn]: {
     name: "Simplilearn",
-    image: null,
   },
   [PlatformEnum.Globalalumni]: {
     name: "Global Alumni",
-    image: null,
   },
   [PlatformEnum.Susskind]: {
     name: "Susskind",
-    image: null,
   },
   [PlatformEnum.Whu]: {
     name: "WHU",
-    image: null,
   },
   [PlatformEnum.Oll]: {
     name: "Open Learning Library",
     image: "/platform_logos/oll.svg",
+    aspect: 5.25
   },
   [PlatformEnum.Youtube]: {
     name: "YouTube",
-    image: null,
   },
 }
+
+const DEFAULT_WIDTH = 200
 
 export const PlatformLogo: React.FC<{
   platformCode?: PlatformEnum
   className?: string
-}> = ({ platformCode, className }) => {
+  width?: number
+  height?: number
+}> = ({ platformCode, className, width, height }) => {
   const platform = PLATFORMS[platformCode!]
   if (!platform?.image) {
     return null
   }
+
+  /* The Next.js Image component's requirement to specify
+   * both width and height are peculiar in the context of SVG
+   * images that do not optimize. The @next/next/no-img-element
+   * lint rule does not have any escape for SVGs despite the
+   * warning not applying - "Using `<img>` could result in slower
+   * LCP and higher bandwidth.".
+   */
+  if (width && !height) {
+    height = width / platform.aspect
+  }
+  if (!width && height) {
+    width = height * platform.aspect
+  }
+  if (!width) {
+    width = DEFAULT_WIDTH
+    height = width / platform.aspect
+  }
+
   return (
-    <img
+    <Image
       src={`/images${platform?.image}`}
       className={className}
       alt={platform.name}
+      width={width}
+      height={height}
     />
   )
 }
