05e2e52e2c1221840655136a179b15f6fbc0c566
Nathan Levesque <nlevesq@mit.edu>
2024-10-02 21:43:54 -0400

    Generate schema

--- frontends/api/src/generated/v0/api.ts
+++ frontends/api/src/generated/v0/api.ts
@@ -2150,6 +2150,18 @@ export interface ProgramCertificate {
    * @type {string}
    * @memberof ProgramCertificate
    */
+  program_letter_generate_url: string
+  /**
+   *
+   * @type {string}
+   * @memberof ProgramCertificate
+   */
+  program_letter_share_url: string
+  /**
+   *
+   * @type {string}
+   * @memberof ProgramCertificate
+   */
   program_title: string
   /**
    *
--- frontends/api/src/generated/v1/api.ts
+++ frontends/api/src/generated/v1/api.ts
@@ -4839,6 +4839,18 @@ export interface ProgramCertificate {
    * @type {string}
    * @memberof ProgramCertificate
    */
+  program_letter_generate_url: string
+  /**
+   *
+   * @type {string}
+   * @memberof ProgramCertificate
+   */
+  program_letter_share_url: string
+  /**
+   *
+   * @type {string}
+   * @memberof ProgramCertificate
+   */
   program_title: string
   /**
    *
