cbd01a0dd4ebb20308905a5efc3c1aadd62c2a64
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-10-04 13:45:52 -0400

    move ecommerce to nextjs

--- frontends/main/src/app-pages/EcommercePages/CartPage/CartPage.tsx
+++ frontends/main/src/app-pages/EcommercePages/CartPage/CartPage.tsx
@@ -1,14 +1,13 @@
+"use client"
 import React from "react"
 import { Breadcrumbs, Container, Typography } from "ol-components"
 import EcommerceFeature from "@/page-components/EcommerceFeature/EcommerceFeature"
-import MetaTags from "@/page-components/MetaTags/MetaTags"
 import * as urls from "@/common/urls"
 
 const CartPage: React.FC = () => {
   return (
     <EcommerceFeature>
       <Container>
-        <MetaTags title="Shopping Cart" />
         <Breadcrumbs
           variant="light"
           ancestors={[{ href: urls.HOME, label: "Home" }]}
