c4a1c5f3b965ab8e5b109f065a88236ac5d70533
shankar ambady <ambady@mit.edu>
2024-10-03 20:39:56 -0400

    removing backend randomization

--- learning_resources/views.py
+++ learning_resources/views.py
@@ -1080,8 +1080,8 @@ class FeaturedViewSet(
         queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
         if page is not None:
-            serializer = self.get_serializer(self._randomize_results(page), many=True)
+            serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer = self.get_serializer(self._randomize_results(queryset), many=True)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
