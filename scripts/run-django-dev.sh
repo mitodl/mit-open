b8765d916646c3a5c2d0ef7488d0efdbd32cc0c8
shankar ambady <ambady@mit.edu>
2024-10-10 11:47:51 -0400

    adding defaults to get or create

--- scripts/run-django-dev.sh
+++ scripts/run-django-dev.sh
@@ -3,8 +3,14 @@
 # This script runs the django app
 
 python3 manage.py collectstatic --noinput --clear
+
+# run initial django migrations
 python3 manage.py migrate --noinput
+
+# populate cache table
 python3 manage.py createcachetable
+
+# run ONLY data migrations
 RUN_DATA_MIGRATIONS=true python3 manage.py migrate --noinput
 
 # load required fixtures on development by default
