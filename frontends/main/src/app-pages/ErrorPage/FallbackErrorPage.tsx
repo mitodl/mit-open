3f85f0fe2e01b0637b52ab1e37b834b87c1d927e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-10-16 15:24:13 -0400

    cleanup

--- frontends/main/src/app-pages/ErrorPage/FallbackErrorPage.tsx
+++ frontends/main/src/app-pages/ErrorPage/FallbackErrorPage.tsx
@@ -4,19 +4,15 @@ import React from "react"
 import ErrorPageTemplate from "./ErrorPageTemplate"
 import { Typography } from "ol-components"
 
-// Double check type Error below
-// Sentry wizard thinks it should be
-// import Error from "next/error";
-
-const GlobalErrorPage = ({ error }: { error: Pick<Error, "message"> }) => {
+const FallbackErrorPage = ({ error }: { error: Pick<Error, "message"> }) => {
   return (
     <ErrorPageTemplate title="Unexpected Error">
       <Typography variant="h3" component="h1">
         Something went wrong.
       </Typography>
-      {error.message || ""}
+      {error.message}
     </ErrorPageTemplate>
   )
 }
 
-export default GlobalErrorPage
+export default FallbackErrorPage
