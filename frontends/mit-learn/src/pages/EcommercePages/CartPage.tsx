5295977e74951b9f8a6d68140a8c2442d47f1988
James Kachel <jkachel@mit.edu>
2024-10-17 15:24:00 -0500

    (no-verify) adding some basic structure and component stubs to the cart page

--- frontends/mit-learn/src/pages/EcommercePages/CartPage.tsx
+++ frontends/mit-learn/src/pages/EcommercePages/CartPage.tsx
@@ -1,8 +1,56 @@
 import React from "react"
-import { Breadcrumbs, Container, Typography } from "ol-components"
+import { Breadcrumbs, Card, Container, Typography, styled } from "ol-components"
 import EcommerceFeature from "@/page-components/EcommerceFeature/EcommerceFeature"
 import MetaTags from "@/page-components/MetaTags/MetaTags"
 import * as urls from "@/common/urls"
+
+const CartContainer = styled(Container)(({ theme }) => ({
+  display: "flex",
+  [theme.breakpoints.down("md")]: {
+    padding: "24px 16px",
+    gap: "24px",
+  },
+}))
+
+const CartItemsContainer = styled(Container)(() => ({
+  width: "66%",
+}))
+
+const OrderSummaryContainer = styled(Container)(() => ({
+  width: "33%",
+}))
+
+const ItemImg = styled.img`
+  padding: 32px;
+  padding-right: 0;
+  width: 200px;
+  height: auto;
+`
+
+const ItemDetails = styled.div`
+  width: auto;
+  flex-grow: 1;
+  padding: 32px;
+`
+
+const ItemCard: React.FC = () => <>
+  <ItemImg alt="placeholder" src="http://placecats.com/200/100" />
+  <ItemDetails>
+    <p>Item Type</p>
+    <h3>Item Title</h3>
+
+    <p>Item Details | Remove</p>
+  </ItemDetails>
+</>
+
+const StyledItemCard = styled(ItemCard)(() => ({
+  display: "flex",
+  ["img"]: {
+    padding: "32px",
+    paddingRight: "0",
+    width: "200px",
+  }
+}))
 
 const CartPage: React.FC = () => {
   return (
@@ -19,10 +67,19 @@ const CartPage: React.FC = () => {
           Shopping Cart
         </Typography>
 
-        <Typography>
-          The shopping cart layout should go here, if you're allowed to see
-          this.
+        <Typography component="h2">
+          You are about to purchase the following:
         </Typography>
+
+        <CartContainer>
+          <CartItemsContainer>
+            <StyledItemCard />
+          </CartItemsContainer>
+
+          <OrderSummaryContainer>
+            <h2>Here's where the order summary should go.</h2>
+          </OrderSummaryContainer>
+        </CartContainer>
       </Container>
     </EcommerceFeature>
   )
