d1d4e2448346a2424f2f37c166054e9958e5ad2e
Carey Gumaer <gumaerc@mit.edu>
2024-10-18 13:21:45 -0400

    reorganize top level layout and remove lorem ipsum

--- frontends/ol-components/src/components/LearningResourceExpanded/LearningResourceExpanded.tsx
+++ frontends/ol-components/src/components/LearningResourceExpanded/LearningResourceExpanded.tsx
@@ -45,6 +45,29 @@ const TitleSectionContainer = styled.div({
   backgroundColor: theme.custom.colors.white,
 })
 
+const ContentContainer = styled.div({
+  display: "flex",
+  alignItems: "flex-start",
+  gap: "32px",
+  alignSelf: "stretch",
+})
+
+const LeftContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  gap: "24px",
+  flex: "1 0 0",
+})
+
+const RightContainer = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "flex-start",
+  gap: "24px",
+})
+
 const Image = styled.img<{ aspect: number }>`
   aspect-ratio: ${({ aspect }) => aspect};
   border-radius: 8px;
@@ -92,7 +115,8 @@ const Detail = styled.section`
 
 const Description = styled.p`
   ${{ ...theme.typography.body2 }}
-  color: ${theme.custom.colors.darkGray2};
+  lineHeight: "20px";
+  color: ${theme.custom.colors.black};
   margin: 0;
   white-space: pre-line;
 `
@@ -287,20 +311,8 @@ const CallToActionSection = ({
 const DetailSection = ({ resource }: { resource?: LearningResource }) => {
   return (
     <Detail>
-      <ResourceTitle resource={resource} />
       <ResourceDescription resource={resource} />
     </Detail>
-  )
-}
-
-const ResourceTitle = ({ resource }: { resource?: LearningResource }) => {
-  if (!resource) {
-    return <Skeleton variant="text" height={20} width="66%" />
-  }
-  return (
-    <Typography variant="h5" component="h2">
-      {resource.title}
-    </Typography>
   )
 }
 
@@ -362,68 +374,22 @@ const LearningResourceExpanded: React.FC<LearningResourceExpandedProps> = ({
   return (
     <Container padTop={isVideo}>
       <TitleSection resource={resource} closeDrawer={closeDrawer} />
-
-      <ImageSection resource={resource} config={imgConfig} />
-      <CallToActionSection resource={resource} hide={isVideo} />
-      <DetailSection resource={resource} />
-      <InfoSection
-        resource={resource}
-        run={selectedRun}
-        user={user}
-        onAddToLearningPathClick={onAddToLearningPathClick}
-        onAddToUserListClick={onAddToUserListClick}
-      />
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        felis nisl, imperdiet ac eros ac, vestibulum sagittis magna. Phasellus
-        nunc lacus, rutrum a justo vitae, ultrices elementum sem. Nulla id
-        semper erat, ac mollis diam. Duis erat eros, tempus vitae dui ac,
-        elementum interdum enim. Donec ut laoreet justo, in sagittis metus.
-        Phasellus nec convallis est, ac varius libero. Vivamus fringilla nunc
-        sed felis tempor pulvinar. In hac habitasse platea dictumst. Aenean
-        elementum nec ligula ut bibendum. Nam eu aliquam quam. Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit. Curabitur vestibulum est vitae
-        ante fermentum, vel commodo risus aliquam. Praesent faucibus nisi a
-        pharetra pulvinar. Phasellus eu bibendum urna. Sed nec congue diam, ut
-        pellentesque ante. Quisque venenatis id massa sed facilisis. Orci varius
-        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-        mus. Morbi nibh mauris, elementum at augue a, consectetur viverra ante.
-        Sed placerat velit felis, maximus pretium arcu euismod a. Duis eget mi
-        massa. Vestibulum eget odio eleifend erat efficitur lobortis. Etiam quis
-        tellus justo. Morbi volutpat bibendum ipsum hendrerit varius.
-        Pellentesque faucibus augue nunc, non venenatis sem fringilla id. Sed
-        eget lorem sed nibh rhoncus accumsan. Nulla purus nibh, maximus et
-        consectetur eu, feugiat sed metus. Proin et egestas neque. Praesent
-        aliquam dui vel sem suscipit, eleifend bibendum lorem sagittis. Nam quis
-        metus nisl. Nam laoreet sagittis massa, in scelerisque odio fringilla
-        ac. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        malesuada felis at dui scelerisque egestas. Donec magna risus, mollis eu
-        neque commodo, consequat tincidunt ex. Phasellus luctus condimentum
-        magna auctor fermentum. Etiam aliquam justo et sagittis lacinia. Etiam
-        vestibulum bibendum tortor non molestie. Orci varius natoque penatibus
-        et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi.
-        Integer elementum dolor sed vulputate faucibus. Curabitur mattis
-        convallis risus a fringilla. Curabitur viverra turpis sed lectus rhoncus
-        luctus id nec dui. Nunc auctor vitae ligula a dictum. Nunc et metus in
-        mi ultricies rhoncus in vel leo. Donec risus sapien, elementum sed orci
-        eu, eleifend lobortis tellus. Mauris sit amet lorem est. Nunc eu
-        vehicula ipsum, a semper diam. Sed pretium sem tempus tellus imperdiet
-        dictum. Pellentesque sit amet vehicula lacus. Morbi gravida, ante vel
-        varius dignissim, massa tellus tincidunt libero, non interdum mauris
-        nulla vel nisi. Ut venenatis porttitor porta. Quisque tempus feugiat mi
-        mollis commodo. Nunc ut libero ut arcu dictum cursus semper consequat
-        mi. Morbi tempor finibus turpis, id tincidunt erat congue luctus. Aenean
-        sollicitudin vel mauris quis ultricies. Proin sit amet quam a mauris
-        maximus faucibus. Morbi sed justo vulputate, malesuada sapien vel,
-        vulputate nisi. Aliquam sit amet mauris non eros cursus aliquet. Morbi
-        in purus sit amet nulla dictum tincidunt. Nulla commodo ligula vel lorem
-        facilisis, at varius tortor tristique. Sed laoreet sem vitae scelerisque
-        finibus. Nunc a libero quis sem tristique sagittis dignissim a erat.
-        Curabitur quis sem eget neque ultricies rutrum eget eget augue.
-        Curabitur non lacus venenatis, tristique lacus fermentum, volutpat sem.
-        Cras lobortis risus id leo ultrices, ut varius nulla porta. Proin a
-        vulputate leo. Nullam lacinia faucibus velit vel aliquet.
-      </div>
+      <ContentContainer>
+        <LeftContainer>
+          <DetailSection resource={resource} />
+          <InfoSection
+            resource={resource}
+            run={selectedRun}
+            user={user}
+            onAddToLearningPathClick={onAddToLearningPathClick}
+            onAddToUserListClick={onAddToUserListClick}
+          />
+        </LeftContainer>
+        <RightContainer>
+          <ImageSection resource={resource} config={imgConfig} />
+          <CallToActionSection resource={resource} hide={isVideo} />
+        </RightContainer>
+      </ContentContainer>
     </Container>
   )
 }
